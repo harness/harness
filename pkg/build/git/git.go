a666825af301e4be8eb5028ede900069862ee6a9
Brad Rydzewski <brad.rydzewski@gmail.com>
2014-02-17 22:41:56 -0700

    removed unused migration sample file

--- pkg/build/git/git.go
+++ pkg/build/git/git.go
@@ -7,7 +7,16 @@ const (
 // Git stores the configuration details for
 // executing Git commands.
 type Git struct {
+	// Depth options instructs git to create a shallow
+	// clone with a history truncated to the specified
+	// number of revisions.
 	Depth *int `yaml:"depth,omitempty"`
+
+	// The name of a directory to clone into.
+	// TODO this still needs to be implemented. this field is
+	//      critical for forked Go projects, that need to clone
+	//      to a specific repository.
+	Path string `yaml:"path,omitempty"`
 }
 
 // GitDepth returns GitDefaultDepth
