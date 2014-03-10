81cf496811bbb0884ef8a4a3e2767a3e077db689
Nurahmadie <nurahmadie@gmail.com>
2014-03-10 07:08:58 +0700

    Refactor migration.
    Add `ChangeColumn`, `AddIndex`, `DropIndex` method to
    Operation interface.

---
+++ pkg/database/migrate/api.go
@@ -0,0 +1,36 @@
+package migrate
+
+import (
+	"database/sql"
+)
+
+// Operation interface covers basic migration operations.
+// Implementation details is specific for each database,
+// see migrate/sqlite.go for implementation reference.
+type Operation interface {
+	CreateTable(tableName string, args []string) (sql.Result, error)
+
+	RenameTable(tableName, newName string) (sql.Result, error)
+
+	DropTable(tableName string) (sql.Result, error)
+
+	AddColumn(tableName, columnSpec string) (sql.Result, error)
+
+	ChangeColumn(tableName, columnName, newType string) (sql.Result, error)
+
+	DropColumns(tableName string, columnsToDrop []string) (sql.Result, error)
+
+	RenameColumns(tableName string, columnChanges map[string]string) (sql.Result, error)
+
+	AddIndex(tableName string, columns []string, flag string) (sql.Result, error)
+
+	DropIndex(tableName string, columns[]string) (sql.Result, error)
+}
+
+type MigrationDriver struct {
+	Tx *sql.Tx
+	Operation
+}
+
+type DriverBuilder func(tx *sql.Tx) *MigrationDriver
+
--- pkg/database/migrate/migrate.go
+++ pkg/database/migrate/migrate.go
@@ -48,33 +48,10 @@ INSERT INTO migration (revision) VALUES (?)
 const deleteRevisionStmt = `
 DELETE FROM migration where revision = ?
 `
-
-// Operation interface covers basic migration operations.
-// Implementation details is specific for each database,
-// see migrate/sqlite.go for implementation reference.
-type Operation interface {
-	CreateTable(tableName string, args []string) (sql.Result, error)
-
-	RenameTable(tableName, newName string) (sql.Result, error)
-
-	DropTable(tableName string) (sql.Result, error)
-
-	AddColumn(tableName, columnSpec string) (sql.Result, error)
-
-	DropColumns(tableName string, columnsToDrop []string) (sql.Result, error)
-
-	RenameColumns(tableName string, columnChanges map[string]string) (sql.Result, error)
-}
-
 type Revision interface {
 	Up(mg *MigrationDriver) error
 	Down(mg *MigrationDriver) error
 	Revision() int64
-}
-
-type MigrationDriver struct {
-	Tx *sql.Tx
-	Operation
 }
 
 type Migration struct {
@@ -84,7 +61,7 @@ type Migration struct {
 
 type DriverFunction func(tx *sql.Tx) *MigrationDriver
 
-var Driver DriverFunction
+var Driver DriverBuilder
 
 func New(db *sql.DB) *Migration {
 	return &Migration{db: db}
