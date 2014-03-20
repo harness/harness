b60743156e3c4363c9277767b34d5ad45d5a5371
indexzero <charlie.robbins@gmail.com>
2014-03-20 01:35:14 -0400

    [api] First pass at "jitsu deploy". Thanks @fiveisprime!

--- pkg/plugin/deploy/nodejitsu.go
+++ pkg/plugin/deploy/nodejitsu.go
@@ -1,12 +1,23 @@
 package deploy
 
 import (
+	"fmt"
 	"github.com/drone/drone/pkg/build/buildfile"
 )
 
 type Nodejitsu struct {
+	App   string `yaml:"app,omitempty"`
+	User  string `yaml:"user,omitempty"`
+	Token string `yaml:"token,omitempty"`
 }
 
 func (n *Nodejitsu) Write(f *buildfile.Buildfile) {
+	f.WriteEnv("username", m.User)
+	f.WriteEnv("apiToken", m.Token)
 
+	// Install the jitsu command line interface then
+	// deploy the configured app.
+	f.WriteCmdSilent("[ -f /usr/bin/sudo ] || npm install -g jitsu")
+	f.WriteCmdSilent("[ -f /usr/bin/sudo ] && sudo npm install -g jitsu")
+	f.WriteCmd("jitsu deploy")
 }
