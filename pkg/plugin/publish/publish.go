ec390c320ecc66f0477b76f0678ff47195ab5819
Matt Martz <matt@sivel.net>
2014-03-26 08:07:16 -0500

    Add OpenStack swift publish functionality

--- pkg/plugin/publish/publish.go
+++ pkg/plugin/publish/publish.go
@@ -9,10 +9,14 @@ import (
 // a Build has succeeded
 type Publish struct {
 	S3 *S3 `yaml:"s3,omitempty"`
+	Swift *Swift `yaml:"swift,omitempty"`
 }
 
 func (p *Publish) Write(f *buildfile.Buildfile) {
 	if p.S3 != nil {
 		p.S3.Write(f)
+	}
+	if p.Swift != nil {
+		p.Swift.Write(f)
 	}
 }
