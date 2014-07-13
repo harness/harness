c20f56333034e39fdcb26d07f0be1397ba7ba424
Brad Rydzewski <brad.rydzewski@gmail.com>
2014-07-13 16:04:10 -0700

    reverted back to original sqlite database location

--- server/main.go
+++ server/main.go
@@ -3,11 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"io/ioutil"
 	"net/http"
-	"os"
-	"os/user"
-	"path/filepath"
 	"runtime"
 	"strings"
 
@@ -154,74 +150,3 @@ func setupQueue() {
 func setupHandlers() {
 
 }
-
-// initialize the .drone directory and create a skeleton config
-// file if one does not already exist.
-func init() {
-	// load the current user
-	u, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-
-	// set .drone home dir
-	home = filepath.Join(u.HomeDir, ".drone")
-
-	// create the .drone home directory
-	os.MkdirAll(home, 0777)
-
-	// check for the config file
-	filename := filepath.Join(u.HomeDir, ".drone", "config.toml")
-	if _, err := os.Stat(filename); err != nil {
-		// if not exists, create
-		ioutil.WriteFile(filename, []byte(defaultConfig), 0777)
-	}
-}
-
-var defaultConfig = `
-# Enables user self-registration. If false, the system administrator
-# will need to manually add users to the system.
-registration = true
-
-[smtp]
-host = ""
-port = ""
-from = ""
-username = ""
-password = ""
-
-[bitbucket]
-url = "https://bitbucket.org"
-api = "https://bitbucket.org"
-client = ""
-secret = ""
-enabled = false
-
-[github]
-url = "https://github.com"
-api = "https://api.github.com"
-client = ""
-secret = ""
-enabled = false
-
-[githubenterprise]
-url = ""
-api = ""
-client = ""
-secret = ""
-enabled = false
-
-[gitlab]
-url = ""
-api = ""
-client = ""
-secret = ""
-enabled = false
-
-[stash]
-url = ""
-api = ""
-client = ""
-secret = ""
-enabled = false
-`
