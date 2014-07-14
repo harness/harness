9a2fd73210807cceab82f5c184eef4ef5d2b2af4
Brad Rydzewski <brad.rydzewski@gmail.com>
2014-07-13 20:24:03 -0700

    style mods to the login page

--- server/app/scripts/controllers/sync.js
+++ server/app/scripts/controllers/sync.js
@@ -2,8 +2,8 @@
 
 angular.module('app').controller("SyncController", function($scope, $http, $interval, $location, users) {
 	var stop = $interval(function() {
-		// todo(bradrydzewski) We should poll the user to see
-		// if the sync process is complete.
+		// todo(bradrydzewski) We should poll the user to see if the
+		// sync process is complete, using the user.syncing variable.
 		$interval.cancel(stop);
 		$location.path("/");
 	}, 5000);
