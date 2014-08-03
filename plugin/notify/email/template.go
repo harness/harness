994b6f09942cbfc374686fa677a7c4c99fa61986
Brad Rydzewski <brad.rydzewski@gmail.com>
2014-08-03 14:45:50 -0700

    added email notifications

---
+++ plugin/notify/email/template.go
@@ -0,0 +1,36 @@
+package email
+
+import (
+	"html/template"
+)
+
+// raw email message template
+var rawMessage = `From: %s
+To: %s
+Subject: %s
+MIME-version: 1.0
+Content-Type: text/html; charset="UTF-8"
+
+%s`
+
+// default success email template
+var successTemplate = template.Must(template.New("_").Parse(`
+<p><b>Build was Successful</b></p>
+<p>Repository : {{.Repo.Owner}}/{{.Repo.Name}}</p>
+<p>Commit     : {{.Commit.ShaShort}}</p>
+<p>Author     : {{.Commit.Author}}</p>
+<p>Branch     : {{.Commit.Branch}}</p>
+<p>Message:</p>
+<p>{{ .Commit.Message }}</p>
+`))
+
+// default failure email template
+var failureTemplate = template.Must(template.New("_").Parse(`
+<p><b>Build Failed</b></p>
+<p>Repository : {{.Repo.Owner}}/{{.Repo.Name}}</p>
+<p>Commit     : {{.Commit.ShaShort}}</p>
+<p>Author     : {{.Commit.Author}}</p>
+<p>Branch     : {{.Commit.Branch}}</p>
+<p>Message:</p>
+<p>{{ .Commit.Message }}</p>
+`))
