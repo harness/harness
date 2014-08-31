d92bbfe3f4ec666f6b745b34b60112922b2d2d31
Kirill Zaitsev <kirik910@gmail.com>
2014-08-31 12:15:15 +0400

    Check missing variables

--- server/database/connection/connection.go
+++ server/database/connection/connection.go
@@ -1,6 +1,7 @@
 package connection
 
 import (
+	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -20,6 +21,18 @@ func NewConnection() *Connection {
 }
 
 func (c *Connection) Open() *Connection {
+	wd, _ := os.Getwd()
+
+	if os.Getenv("DRONE_DRIVER") == "" {
+		os.Setenv("DRONE_DRIVER", "sqlite3")
+		log.Println("WARNING: env variable DRONE_DRIVER is missing, use:", os.Getenv("DRONE_DRIVER"))
+	}
+
+	if os.Getenv("DRONE_DATASOURCE") == "" {
+		os.Setenv("DRONE_DATASOURCE", wd+"/drone.sqlite")
+		log.Println("WARNING: env variable DRONE_DATASOURCE is missing, use:", os.Getenv("DRONE_DATASOURCE"))
+	}
+
 	db, err := gorm.Open(os.Getenv("DRONE_DRIVER"), os.Getenv("DRONE_DATASOURCE"))
 	if err != nil {
 		panic(err)
