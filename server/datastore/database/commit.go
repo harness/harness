8a2331857ed0d532ca66059bfc37d096c87486c0
Nils Werner <nils.werner@gmail.com>
2014-11-15 15:42:43 +0100

    Whops, forgot param placeholder in query

--- server/datastore/database/commit.go
+++ server/datastore/database/commit.go
@@ -122,7 +122,7 @@ WHERE c.repo_id = r.repo_id
 	  AND r.repo_id = p.repo_id
 	  AND p.user_id = ?
 	GROUP BY r.repo_id
-) ORDER BY c.commit_created DESC LIMIT 5;
+) ORDER BY c.commit_created DESC LIMIT ?;
 `
 
 // SQL query to retrieve the ungrouped, latest Commits
