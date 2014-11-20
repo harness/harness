702dcb97fe69469929c099f6488cf4d027d9e8f3
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-11-20 15:18:12 +0500

    Add hostname in docker section of .drone.yml

--- shared/build/script/docker_test.go
+++ shared/build/script/docker_test.go
@@ -38,3 +38,32 @@ func TestDockerNetworkMode(t *testing.T) {
         t.Errorf("The result is invalid. [expected: %s][actual: %s]", expected, actual)
     }
 }
+
+func TestDockerHostname(t *testing.T) {
+    var d *Docker
+    var expected string
+
+    expected = ""
+    d = nil
+    if actual := DockerHostname(d); actual != expected {
+        t.Errorf("The result is invalid. [expected: %s][actual: %s]", expected, actual)
+    }
+
+    expected = ""
+    d = &Docker{}
+    if actual := DockerHostname(d); actual != expected {
+        t.Errorf("The result is invalid. [expected: %s][actual: %s]", expected, actual)
+    }
+
+    expected = ""
+    d = &Docker{Hostname: nil}
+    if actual := DockerHostname(d); actual != expected {
+        t.Errorf("The result is invalid. [expected: %s][actual: %s]", expected, actual)
+    }
+
+    expected = "host"
+    d = &Docker{Hostname: &expected}
+    if actual := DockerHostname(d); actual != expected {
+        t.Errorf("The result is invalid. [expected: %s][actual: %s]", expected, actual)
+    }
+}
