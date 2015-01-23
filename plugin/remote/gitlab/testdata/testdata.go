1e6b86d18d3f1541efb9702c5933c2f2ff897357
Kirill Zaitsev <kirik910@gmail.com>
2015-01-23 21:51:37 +0300

    Gitlab oauth login

--- plugin/remote/gitlab/testdata/testdata.go
+++ plugin/remote/gitlab/testdata/testdata.go
@@ -27,6 +27,12 @@ func NewServer() *httptest.Server {
 		case "/api/v3/session":
 			w.Write(sessionPayload)
 			return
+		case "/oauth/token":
+			w.Write(accessTokenPayload)
+			return
+		case "/api/v3/user":
+			w.Write(currentUserPayload)
+			return
 		}
 
 		// else return a 404
@@ -229,5 +235,30 @@ var droneYamlPayload = []byte(`
 	"name":     ".drone.yml",
 	"path":     ".drone.yml",
 	"content":  "aW1hZ2U6IGdv"
+}
+`)
+
+var accessTokenPayload = []byte(`access_token=sekret&scope=api&token_type=bearer`)
+
+var currentUserPayload = []byte(`
+{
+  "id": 1,
+  "username": "john_smith",
+  "email": "john@example.com",
+  "name": "John Smith",
+  "private_token": "dd34asd13as",
+  "state": "active",
+  "created_at": "2012-05-23T08:00:58Z",
+  "bio": null,
+  "skype": "",
+  "linkedin": "",
+  "twitter": "",
+  "website_url": "",
+  "theme_id": 1,
+  "color_scheme_id": 2,
+  "is_admin": false,
+  "can_create_group": true,
+  "can_create_project": true,
+  "projects_limit": 100
 }
 `)
