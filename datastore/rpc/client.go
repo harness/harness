f161155f86f97f254df008d575bf5daab646b9c6
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-04-07 22:10:44 -0700

    added example RPC client

---
+++ datastore/rpc/client.go
@@ -0,0 +1,23 @@
+package rpc
+
+import (
+	"net"
+	"net/rpc"
+)
+
+type Client struct {
+	*rpc.Client
+}
+
+// New returns a new, remote datastore backend that connects
+// via tcp and exchanges data using Go's RPC mechanism.
+func New(address string) (*Client, error) {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return nil, err
+	}
+	client := &Client{
+		rpc.NewClient(conn),
+	}
+	return client, nil
+}
