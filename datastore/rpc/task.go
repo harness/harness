374e6b20b632484366c6f275360743feff1882eb
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-04-07 22:10:44 -0700

    added example RPC client

---
+++ datastore/rpc/task.go
@@ -0,0 +1,78 @@
+package rpc
+
+import (
+	"github.com/drone/drone/common"
+)
+
+type GetTaskReq struct {
+	Repo  string
+	Build int
+	Task  int
+}
+
+type GetTaskResp struct {
+	Task *common.Task
+}
+
+func (c *Client) GetTask(repo string, build int, task int) (*common.Task, error) {
+	req := &GetTaskReq{repo, build, task}
+	res := &GetTaskResp{}
+	err := c.Call("Datastore.GetTask", req, res)
+	return res.Task, err
+}
+
+type GetTaskLogsReq struct {
+	Repo  string
+	Build int
+	Task  int
+}
+
+type GetTaskLogsResp struct {
+	Logs []byte
+}
+
+func (c *Client) GetTaskLogs(repo string, build int, task int) ([]byte, error) {
+	req := &GetTaskLogsReq{repo, build, task}
+	res := &GetTaskLogsResp{}
+	err := c.Call("Datastore.GetTaskLogs", req, res)
+	return res.Logs, err
+}
+
+type GetTaskListReq struct {
+	Repo  string
+	Build int
+}
+
+type GetTaskListResp struct {
+	Tasks []*common.Task
+}
+
+func (c *Client) GetTaskList(repo string, build int) ([]*common.Task, error) {
+	req := &GetTaskListReq{repo, build}
+	res := &GetTaskListResp{}
+	err := c.Call("Datastore.GetTaskList", req, res)
+	return res.Tasks, err
+}
+
+type UpsertTaskReq struct {
+	Repo  string
+	Build int
+	Task  *common.Task
+}
+
+func (c *Client) UpsertTask(repo string, build int, task *common.Task) error {
+	req := &UpsertTaskReq{repo, build, task}
+	return c.Call("Datastore.UpsertTask", req, nil)
+}
+
+type UpsertTaskLogsReq struct {
+	Repo  string
+	Build int
+	Task  int
+	Logs  []byte
+}
+
+func (c *Client) UpsertTaskLogs(repo string, build int, task int, log []byte) error {
+	req := &UpsertTaskLogsReq{repo, build, task, log}
+	return c.Call("Datastore.UpsertTaskLogs", req, nil)
+}
---
+++ datastore/rpc/token.go
@@ -0,0 +1,38 @@
+package rpc
+
+import (
+	"github.com/drone/drone/common"
+)
+
+type GetTokenReq struct {
+	Sha string
+}
+
+type GetTokenResp struct {
+	Token *common.Token
+}
+
+func (c *Client) GetToken(sha string) (*common.Token, error) {
+	req := &GetTokenReq{sha}
+	res := &GetTokenResp{}
+	err := c.Call("Datastore.GetToken", req, res)
+	return res.Token, err
+}
+
+type InsertTokenReq struct {
+	Token *common.Token
+}
+
+func (c *Client) InsertToken(token *common.Token) error {
+	req := &InsertTokenReq{token}
+	return c.Call("Datastore.InsertToken", req, nil)
+}
+
+type DeleteTokenReq struct {
+	Token *common.Token
+}
+
+func (c *Client) DeleteToken(token *common.Token) error {
+	req := &DeleteTokenReq{token}
+	return c.Call("Datastore.DeleteToken", req, nil)
+}
