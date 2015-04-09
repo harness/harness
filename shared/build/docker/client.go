b04b1927be7d5b66862127ce11f43b3730ecca5e
Alexey Chernenkov <laise@pisem.net>
2015-04-09 17:12:01 +0500

    Fix dependency: github.com/docker/docker/utils was moved to pkg/jsonmessage

--- shared/build/docker/client.go
+++ shared/build/docker/client.go
@@ -15,9 +15,9 @@ import (
 	"strings"
 	"time"
 
+	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/docker/docker/pkg/term"
-	"github.com/docker/docker/utils"
 )
 
 const (
@@ -353,7 +353,7 @@ func (c *Client) stream(method, path string, in io.Reader, out io.Writer, header
 
 		// it may not make sense to put this code here, but it works for
 		// us at the moment, and I don't feel like refactoring
-		return utils.DisplayJSONMessagesStream(resp.Body, out, terminalFd, isTerminal)
+		return jsonmessage.DisplayJSONMessagesStream(resp.Body, out, terminalFd, isTerminal)
 	}
 	// otherwise plain text
 	if _, err := io.Copy(out, resp.Body); err != nil {
