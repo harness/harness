57188ee4f2ee164887fb5f05184cf4057e40cc7d
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-04-13 01:22:51 -0700

    ability to manage tokens

--- datastore/bolt/token.go
+++ datastore/bolt/token.go
@@ -1,6 +1,8 @@
 package bolt
 
 import (
+	"bytes"
+
 	"github.com/boltdb/bolt"
 	"github.com/drone/drone/common"
 )
@@ -22,16 +24,44 @@ func (db *DB) GetToken(user, label string) (*common.Token, error) {
 func (db *DB) InsertToken(token *common.Token) error {
 	key := []byte(token.Login + "/" + token.Label)
 	return db.Update(func(t *bolt.Tx) error {
+		// gets an index
+		var idx [][]byte
+		err := get(t, bucketUserTokens, []byte(token.Login), &idx)
+		if err != nil && err != ErrKeyNotFound {
+			return err
+		}
+		// pushes to the index
+		idx = append(idx, key)
+		err = update(t, bucketUserTokens, []byte(token.Login), &idx)
+		if err != nil {
+			return err
+		}
+
 		return insert(t, bucketTokens, key, token)
 	})
-	// TODO(bradrydzewski) add token to users_token index
 }
 
 // DeleteUser deletes the token.
 func (db *DB) DeleteToken(token *common.Token) error {
 	key := []byte(token.Login + "/" + token.Label)
 	return db.Update(func(t *bolt.Tx) error {
+		// gets an index
+		var idx [][]byte
+		err := get(t, bucketUserTokens, []byte(token.Login), &idx)
+		if err != nil && err != ErrKeyNotFound {
+			return err
+		}
+		// pushes to the index
+		for i, val := range idx {
+			if bytes.Equal(val, key) {
+				idx = idx[:i+copy(idx[i:], idx[i+1:])]
+				break
+			}
+		}
+		err = update(t, bucketUserTokens, []byte(token.Login), &idx)
+		if err != nil {
+			return err
+		}
 		return delete(t, bucketUser, key)
 	})
-	// TODO(bradrydzewski) remove token from users_token index
 }
--- datastore/bolt/token_test.go
+++ datastore/bolt/token_test.go
@@ -1,17 +1,39 @@
 package bolt
 
 import (
+	"os"
 	"testing"
 
+	"github.com/drone/drone/common"
 	. "github.com/franela/goblin"
 )
 
 func TestToken(t *testing.T) {
 	g := Goblin(t)
 	g.Describe("Tokens", func() {
+		var db *DB // temporary database
 
-		g.It("Should find by sha")
-		g.It("Should list for user")
+		// create a new database before each unit
+		// test and destroy afterwards.
+		g.BeforeEach(func() {
+			db = Must("/tmp/drone.test.db")
+		})
+		g.AfterEach(func() {
+			os.Remove(db.Path())
+		})
+
+		g.It("Should find by label")
+		g.It("Should list for user", func() {
+			db.InsertUser(&common.User{Login: "octocat"})
+			err1 := db.InsertToken(&common.Token{Login: "octocat", Label: "gist"})
+			err2 := db.InsertToken(&common.Token{Login: "octocat", Label: "github"})
+			g.Assert(err1).Equal(nil)
+			g.Assert(err2).Equal(nil)
+
+			list, err := db.GetUserTokens("octocat")
+			g.Assert(err).Equal(nil)
+			g.Assert(len(list)).Equal(2)
+		})
 		g.It("Should delete")
 		g.It("Should insert")
 		g.It("Should not insert if exists")
