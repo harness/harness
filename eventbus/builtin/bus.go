e595d7d334b606878338cb90ae374c1cb61fb03d
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-04-28 15:30:51 -0700

    test cov for eventbus

--- eventbus/builtin/bus.go
+++ eventbus/builtin/bus.go
@@ -1,26 +1,28 @@
-package eventbus
+package builtin
 
 import (
 	"sync"
+
+	"github.com/drone/drone/eventbus"
 )
 
 type EventBus struct {
 	sync.Mutex
-	subs map[chan *Event]bool
+	subs map[chan *eventbus.Event]bool
 }
 
 // New creates a new EventBus that manages a list of
 // subscribers to which events are published.
 func New() *EventBus {
 	return &EventBus{
-		subs: make(map[chan *Event]bool),
+		subs: make(map[chan *eventbus.Event]bool),
 	}
 }
 
 // Subscribe adds the channel to the list of
 // subscribers. Each subscriber in the list will
 // receive broadcast events.
-func (b *EventBus) Subscribe(c chan *Event) {
+func (b *EventBus) Subscribe(c chan *eventbus.Event) {
 	b.Lock()
 	b.subs[c] = true
 	b.Unlock()
@@ -28,19 +30,19 @@ func (b *EventBus) Subscribe(c chan *Event) {
 
 // Unsubscribe removes the channel from the
 // list of subscribers.
-func (b *EventBus) Unsubscribe(c chan *Event) {
+func (b *EventBus) Unsubscribe(c chan *eventbus.Event) {
 	b.Lock()
 	delete(b.subs, c)
 	b.Unlock()
 }
 
 // Send dispatches a message to all subscribers.
-func (b *EventBus) Send(event *Event) {
+func (b *EventBus) Send(event *eventbus.Event) {
 	b.Lock()
 	defer b.Unlock()
 
-	for s, _ := range b.subs {
-		go func(c chan *Event) {
+	for s := range b.subs {
+		go func(c chan *eventbus.Event) {
 			defer recover()
 			c <- event
 		}(s)
