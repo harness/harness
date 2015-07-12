18111dad7a12a97645f9ea3e88716db16b95478c
Alexander Simonov <alex@simonov.me>
2015-07-12 22:32:46 +0300

    Little refactoring of all assets. JS code better identing.

--- cmd/drone-server/static/scripts/controllers/repos.js
+++ cmd/drone-server/static/scripts/controllers/repos.js
@@ -1,115 +1,115 @@
 (function () {
 
-	/**
-	 * ReposCtrl responsible for rendering the user's
-	 * repository home screen.
-	 */
-	function ReposCtrl($scope, $stateParams, repos, users) {
-		// Gets the currently authenticated user
-		users.getCached().then(function(payload){
-			$scope.user = payload.data;
-		});
+  /**
+   * ReposCtrl responsible for rendering the user's
+   * repository home screen.
+   */
+  function ReposCtrl($scope, $stateParams, repos, users) {
+    // Gets the currently authenticated user
+    users.getCached().then(function (payload) {
+      $scope.user = payload.data;
+    });
 
-		// Gets a list of repos to display in the
-		// dropdown.
-		repos.list().then(function(payload){
-			$scope.repos = angular.isArray(payload.data) ? payload.data : [];
-		}).catch(function(err){
-			$scope.error = err;
-		});
-	}
+    // Gets a list of repos to display in the
+    // dropdown.
+    repos.list().then(function (payload) {
+      $scope.repos = angular.isArray(payload.data) ? payload.data : [];
+    }).catch(function (err) {
+      $scope.error = err;
+    });
+  }
 
-	/**
-	 * RepoAddCtrl responsible for activaing a new
-	 * repository.
-	 */
-	function RepoAddCtrl($scope, $location, repos, users) {
+  /**
+   * RepoAddCtrl responsible for activaing a new
+   * repository.
+   */
+  function RepoAddCtrl($scope, $location, repos, users) {
 
-		// Gets the currently authenticated user
-		users.getCached().then(function(payload){
-			$scope.user = payload.data;
-		});
+    // Gets the currently authenticated user
+    users.getCached().then(function (payload) {
+      $scope.user = payload.data;
+    });
 
-		$scope.add = function(slug) {
-			repos.post(slug).then(function(payload) {
-				$location.path('/'+slug);
-			}).catch(function(err){
-				$scope.error = err;
-			});
-		}
-	}
+    $scope.add = function (slug) {
+      repos.post(slug).then(function (payload) {
+        $location.path('/' + slug);
+      }).catch(function (err) {
+        $scope.error = err;
+      });
+    }
+  }
 
-	/**
-	 * RepoEditCtrl responsible for editing a repository.
-	 */
-	function RepoEditCtrl($scope, $window, $location, $stateParams, repos, users) {
-		var owner = $stateParams.owner;
-		var name  = $stateParams.name;
-		var fullName = owner+'/'+name;
+  /**
+   * RepoEditCtrl responsible for editing a repository.
+   */
+  function RepoEditCtrl($scope, $window, $location, $stateParams, repos, users) {
+    var owner = $stateParams.owner;
+    var name = $stateParams.name;
+    var fullName = owner + '/' + name;
 
-		// Inject window for composing url
-		$scope.window = $window;
+    // Inject window for composing url
+    $scope.window = $window;
 
-		// Gets the currently authenticated user
-		users.getCached().then(function(payload){
-			$scope.user = payload.data;
-		});
+    // Gets the currently authenticated user
+    users.getCached().then(function (payload) {
+      $scope.user = payload.data;
+    });
 
-		// Gets a repository
-		repos.get(fullName).then(function(payload){
-			$scope.repo = payload.data;
-		}).catch(function(err){
-			$scope.error = err;
-		});
+    // Gets a repository
+    repos.get(fullName).then(function (payload) {
+      $scope.repo = payload.data;
+    }).catch(function (err) {
+      $scope.error = err;
+    });
 
-		$scope.save = function(repo) {
-			repo.timeout = parseInt(repo.timeout);
-			repos.update(repo).then(function(payload) {
-				$scope.repo = payload.data;
-			}).catch(function(err){
-				$scope.error = err;
-			});
-		}
+    $scope.save = function (repo) {
+      repo.timeout = parseInt(repo.timeout);
+      repos.update(repo).then(function (payload) {
+        $scope.repo = payload.data;
+      }).catch(function (err) {
+        $scope.error = err;
+      });
+    };
 
-		$scope.delete = function(repo) {
-			repos.delete(repo).then(function(payload) {
-				$location.path('/');
-			}).catch(function(err){
-				$scope.error = err;
-			});
-		}
+    $scope.delete = function (repo) {
+      repos.delete(repo).then(function (payload) {
+        $location.path('/');
+      }).catch(function (err) {
+        $scope.error = err;
+      });
+    };
 
-		$scope.param={}
-		$scope.addParam = function(param) {
-			if (!$scope.repo.params) {
-				$scope.repo.params = {}
-			}
-			$scope.repo.params[param.key]=param.value;
-			$scope.param={}
+    $scope.param = {};
+    $scope.addParam = function (param) {
+      if (!$scope.repo.params) {
+        $scope.repo.params = {}
+      }
+      $scope.repo.params[param.key] = param.value;
+      $scope.param = {};
 
-			// auto-update
-			repos.update($scope.repo).then(function(payload) {
-				$scope.repo = payload.data;
-			}).catch(function(err){
-				$scope.error = err;
-			});
-		}
+      // auto-update
+      repos.update($scope.repo).then(function (payload) {
+        $scope.repo = payload.data;
+      }).catch(function (err) {
+        $scope.error = err;
+      });
+    };
 
-		$scope.deleteParam = function(key) {
-			delete $scope.repo.params[key];
+    $scope.deleteParam = function (key) {
+      delete $scope.repo.params[key];
 
-			// auto-update
-			repos.update($scope.repo).then(function(payload) {
-				$scope.repo = payload.data;
-			}).catch(function(err){
-				$scope.error = err;
-			});
-		}
-	}
+      // auto-update
+      repos.update($scope.repo).then(function (payload) {
+        $scope.repo = payload.data;
+      }).catch(function (err) {
+        $scope.error = err;
+      });
+    }
+  }
 
-	angular
-		.module('drone')
-		.controller('ReposCtrl', ReposCtrl)
-		.controller('RepoAddCtrl', RepoAddCtrl)
-		.controller('RepoEditCtrl', RepoEditCtrl);
+  angular
+    .module('drone')
+    .controller('ReposCtrl', ReposCtrl)
+    .controller('RepoAddCtrl', RepoAddCtrl)
+    .controller('RepoEditCtrl', RepoEditCtrl);
 })();
--- cmd/drone-server/static/scripts/drone.js
+++ cmd/drone-server/static/scripts/drone.js
@@ -2,226 +2,230 @@
 
 (function () {
 
-	/**
-	 * Creates the angular application.
-	 */
-	angular.module('drone', [
-			'ngRoute',
-			'ui.filters',
-			'ui.router'
-		]);
+  /**
+   * Creates the angular application.
+   */
+  angular.module('drone', [
+    'ngRoute',
+    'ui.filters',
+    'ui.router'
+  ]);
 
-	/**
-	 * Bootstraps the application and retrieves the
-	 * token from the
-	 */
-	function Authorize() {
-		// First, parse the query string
-		var params = {}, queryString = location.hash.substring(1),
-			regex  = /([^&=]+)=([^&]*)/g, m;
+  /**
+   * Bootstraps the application and retrieves the
+   * token from the
+   */
+  function Authorize() {
+    // First, parse the query string
+    var params = {}, queryString = location.hash.substring(1),
+      regex = /([^&=]+)=([^&]*)/g, m;
 
-		// Loop through and retrieve the token
-		while (m = regex.exec(queryString)) {
-			params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-		}
+    // Loop through and retrieve the token
+    while (m = regex.exec(queryString)) {
+      params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+    }
 
-		// if the user has just received an auth token we
-		// should extract from the URL, save to local storage
-		// and then remove from the URL for good measure.
-		if (params.access_token) {
-			localStorage.setItem("access_token", params.access_token);
-			history.replaceState({}, document.title, location.pathname);
-		}
-	}
+    // if the user has just received an auth token we
+    // should extract from the URL, save to local storage
+    // and then remove from the URL for good measure.
+    if (params.access_token) {
+      localStorage.setItem("access_token", params.access_token);
+      history.replaceState({}, document.title, location.pathname);
+    }
+  }
 
-	/**
-	 * Defines the route configuration for the
-	 * main application.
-	 */
-	function Config ($stateProvider, $httpProvider, $locationProvider) {
+  /**
+   * Defines the route configuration for the
+   * main application.
+   */
+  function Config($stateProvider, $httpProvider, $locationProvider) {
 
-		// Resolver that will attempt to load the currently
-		// authenticated user prior to loading the page.
-		var resolveUser = {
-			user: function(users) {
-				return users.getCached();
-			}
-		}
+    // Resolver that will attempt to load the currently
+    // authenticated user prior to loading the page.
+    var resolveUser = {
+      user: function (users) {
+        return users.getCached();
+      }
+    };
 
-		$stateProvider
-			.state('app', {
-				abstract: true,
-				views: {
-					'layout': { 
-						templateUrl: '/static/scripts/views/layout.html',
-						controller: function ($scope, $routeParams, repos, users) {
-							users.getCached().then(function(payload){
-								$scope.user = payload.data;
-								console.log(repos.list());
-							});
-						}
-					}
-				},
-				resolve: resolveUser
-			})
-			.state('app.index', {
-				url: '/',
-				views: {
-					'toolbar': { 
-						templateUrl: '/static/scripts/views/repos/index/toolbar.html'
-					},
-					'content': { 
-						templateUrl: '/static/scripts/views/repos/index/content.html',
-						controller: 'ReposCtrl',
-						resolve: resolveUser
-					}
-				},
-				title: 'Dashboard'
-			})
-			.state('login', {
-				url: '/login',
-				templateUrl: '/static/scripts/views/login.html',
-				title: 'Login',
-				controller: 'UserLoginCtrl'
-			})
-			.state('app.profile', {
-				url: '/profile',
-				views: {
-					'toolbar': { templateUrl: '/static/scripts/views/profile/toolbar.html' },
-					'content': { 
-						templateUrl: '/static/scripts/views/profile/content.html',
-						controller: 'UserCtrl',
-						resolve: resolveUser
-					}
-				},
-				title: 'Profile'
-			})
-			.state('app.users', {
-				url: '/users',
-				views: {
-					'toolbar': { templateUrl: '/static/scripts/views/users/toolbar.html' },
-					'content': { 
-						templateUrl: '/static/scripts/views/users/content.html',
-						controller: 'UsersCtrl',
-						resolve: resolveUser
-					}
-				},
-				title: 'Users'
-			})
-			.state('app.new_repo', {
-				url: '/new',
-				views: {
-					'toolbar': { templateUrl: '/static/scripts/views/repos/add/toolbar.html' },
-					'content': { 
-						templateUrl: '/static/scripts/views/repos/add/content.html',
-						controller: 'RepoAddCtrl',
-						resolve: resolveUser
-					}
-				},
-				title: 'Add Repository'
-			})
-			.state('app.builds', {
-				url: '/:owner/:name',
-				views: {
-					'toolbar': { 
-						templateUrl: '/static/scripts/views/builds/index/toolbar.html',
-						controller: 'BuildsCtrl'
-					},
-					'content': { 
-						templateUrl: '/static/scripts/views/builds/index/content.html',
-						controller: 'BuildsCtrl'
-					}
-				}
-			})
-			.state('app.repo_edit', {
-				url: '/:owner/:name/edit',
-				views: {
-					'toolbar': { templateUrl: '/static/scripts/views/repos/toolbar.html' },
-					'content': { templateUrl: '/static/scripts/views/repos/edit.html' }
-				},
-				controller: 'RepoEditCtrl',
-				resolve: resolveUser
-			})
-			.state('app.repo.env', {
-				url: '/:owner/:name/edit/env',
-				views: {
-					'toolbar': { templateUrl: '/static/scripts/views/repos/toolbar.html' },
-					'content': { templateUrl: '/static/scripts/views/repos/env.html' }
-				},
-				controller: 'RepoEditCtrl',
-				resolve: resolveUser
-			})
-			.state('app.repo.del', {
-				url: '/:owner/:name/delete',
-				views: {
-					'toolbar': { templateUrl: '/static/scripts/views/repos/toolbar.html' },
-					'content': { templateUrl: '/static/scripts/views/repos/del.html' }
-				},
-				controller: 'RepoEditCtrl',
-				resolve: resolveUser
-			})
-			.state('app.build', {
-				url: '/:owner/:name/:number',
-				views: {
-					'toolbar': { templateUrl: '/static/scripts/views/builds/show/toolbar.html' },
-					'content': { templateUrl: '/static/scripts/views/builds/show/content.html' }
-				},
-				controller: 'BuildCtrl',
-				resolve: resolveUser
-			})
-			.state('app.build_step', {
-				url: '/:owner/:name/:number/:step',
-				views: {
-					'toolbar': { templateUrl: '/static/scripts/views/builds/step/toolbar.html' },
-					'content': { templateUrl: '/static/scripts/views/builds/step/content.html' }
-				},
-				controller: 'BuildOutCtrl',
-				resolve: resolveUser
-			})
+    $stateProvider
+      .state('app', {
+        abstract: true,
+        views: {
+          'layout': {
+            templateUrl: '/static/scripts/views/layout.html',
+            controller: function ($scope, $routeParams, repos, users) {
+              users.getCached().then(function (payload) {
+                $scope.user = payload.data;
+              });
+            }
+          }
+        }
+      })
+      .state('app.index', {
+        url: '/',
+        views: {
+          'toolbar': {
+            templateUrl: '/static/scripts/views/repos/index/toolbar.html'
+          },
+          'content': {
+            templateUrl: '/static/scripts/views/repos/index/content.html',
+            controller: 'ReposCtrl',
+            resolve: resolveUser
+          }
+        },
+        title: 'Dashboard'
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: '/static/scripts/views/login.html',
+        title: 'Login',
+        controller: 'UserLoginCtrl'
+      })
+      .state('app.profile', {
+        url: '/profile',
+        views: {
+          'toolbar': {templateUrl: '/static/scripts/views/profile/toolbar.html'},
+          'content': {
+            templateUrl: '/static/scripts/views/profile/content.html',
+            controller: 'UserCtrl',
+            resolve: resolveUser
+          }
+        },
+        title: 'Profile'
+      })
+      .state('app.users', {
+        url: '/users',
+        views: {
+          'toolbar': {templateUrl: '/static/scripts/views/users/toolbar.html'},
+          'content': {
+            templateUrl: '/static/scripts/views/users/content.html',
+            controller: 'UsersCtrl',
+            resolve: resolveUser
+          }
+        },
+        title: 'Users'
+      })
+      .state('app.new_repo', {
+        url: '/new',
+        views: {
+          'toolbar': {templateUrl: '/static/scripts/views/repos/add/toolbar.html'},
+          'content': {
+            templateUrl: '/static/scripts/views/repos/add/content.html',
+            controller: 'RepoAddCtrl',
+            resolve: resolveUser
+          }
+        },
+        title: 'Add Repository'
+      })
+      .state('app.builds', {
+        url: '/:owner/:name',
+        views: {
+          'toolbar': {
+            templateUrl: '/static/scripts/views/builds/index/toolbar.html',
+            controller: 'BuildsCtrl'
+          },
+          'content': {
+            templateUrl: '/static/scripts/views/builds/index/content.html',
+            controller: 'BuildsCtrl'
+          }
+        }
+      })
+      .state('app.repo_edit', {
+        url: '/:owner/:name/edit',
+        views: {
+          'toolbar': {
+            templateUrl: '/static/scripts/views/repos/toolbar.html',
+            controller: 'RepoEditCtrl',
+            resolve: resolveUser
+          },
+          'content': {
+            templateUrl: '/static/scripts/views/repos/edit.html',
+            controller: 'RepoEditCtrl',
+            resolve: resolveUser
+          }
+        },
+        title: 'Edit Repository'
+      })
+      .state('app.repo.env', {
+        url: '/:owner/:name/edit/env',
+        views: {
+          'toolbar': {templateUrl: '/static/scripts/views/repos/toolbar.html'},
+          'content': {templateUrl: '/static/scripts/views/repos/env.html'}
+        },
+        controller: 'RepoEditCtrl',
+        resolve: resolveUser
+      })
+      .state('app.repo.del', {
+        url: '/:owner/:name/delete',
+        views: {
+          'toolbar': {templateUrl: '/static/scripts/views/repos/toolbar.html'},
+          'content': {templateUrl: '/static/scripts/views/repos/del.html'}
+        },
+        controller: 'RepoEditCtrl',
+        resolve: resolveUser
+      })
+      .state('app.build', {
+        url: '/:owner/:name/:number',
+        views: {
+          'toolbar': {templateUrl: '/static/scripts/views/builds/show/toolbar.html'},
+          'content': {templateUrl: '/static/scripts/views/builds/show/content.html'}
+        },
+        controller: 'BuildCtrl',
+        resolve: resolveUser
+      })
+      .state('app.build_step', {
+        url: '/:owner/:name/:number/:step',
+        views: {
+          'toolbar': {templateUrl: '/static/scripts/views/builds/step/toolbar.html'},
+          'content': {templateUrl: '/static/scripts/views/builds/step/content.html'}
+        },
+        controller: 'BuildOutCtrl',
+        resolve: resolveUser
+      });
 
-		// Enables html5 mode
-		$locationProvider.html5Mode(true)
+    // Enables html5 mode
+    $locationProvider.html5Mode(true);
 
-		// Appends the Bearer token to authorize every
-		// outbound http request.
-		$httpProvider.defaults.headers.common.Authorization = 'Bearer '+localStorage.getItem('access_token');
+    // Appends the Bearer token to authorize every
+    // outbound http request.
+    $httpProvider.defaults.headers.common.Authorization = 'Bearer ' + localStorage.getItem('access_token');
 
-		// Intercepts every oubput http response and redirects
-		// the user to the logic screen if the request was rejected.
-		$httpProvider.interceptors.push(function($q, $location) {
-			return {
-				'responseError': function(rejection) {
-					if (rejection.status === 401 && rejection.config.url !== "/api/user") {
-						$location.path('/login');
-					}
-					if (rejection.status === 0) {
-						// this happens when the app is down or
-						// the browser loses internet connectivity.
-					}
-					return $q.reject(rejection);
-				}
-			};
-		});
-	}
+    // Intercepts every oubput http response and redirects
+    // the user to the logic screen if the request was rejected.
+    $httpProvider.interceptors.push(function ($q, $location) {
+      return {
+        'responseError': function (rejection) {
+          if (rejection.status === 401 && rejection.config.url !== "/api/user") {
+            $location.path('/login');
+          }
+          if (rejection.status === 0) {
+            // this happens when the app is down or
+            // the browser loses internet connectivity.
+          }
+          return $q.reject(rejection);
+        }
+      };
+    });
+  }
 
+  function RouteChange($rootScope, repos, logs) {
+    $rootScope.$on('$stateChangeStart', function () {
+      // repos.unsubscribe();
+      // logs.unsubscribe();
+    });
 
-	function RouteChange($rootScope, repos, logs) {
-		$rootScope.$on('$stateChangeStart', function () {
-			repos.unsubscribe();
-			logs.unsubscribe();
-		});
+    $rootScope.$on('$stateChangeSuccess', function (event, current) {
+      if (current.title) {
+        document.title = current.title + ' · drone';
+      }
+    });
+  }
 
-		$rootScope.$on('$stateChangeSuccess', function (event, current, previous) {
-			if (current.title) {
- 				document.title = current.title + ' · drone';
-			}
-		});
-	}
-
-	angular
-		.module('drone')
-		.config(Authorize)
-		.config(Config)
-		.run(RouteChange);
+  angular
+    .module('drone')
+    .config(Authorize)
+    .config(Config)
+    .run(RouteChange);
 
 })();
