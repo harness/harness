35f36b97fb44fc135085eec934d7110f8b5cd9b9
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-09-30 13:50:12 -0700

    use install -t instead of make install for sassc

--- contrib/setup-sassc.sh
+++ contrib/setup-sassc.sh
@@ -3,14 +3,19 @@ set -e
 
 cd /tmp
 
+# cleanup previously downloaded and unpacked files.
 rm -rf libsass
 rm -rf sassc
 
+# download the latest build of sassc
 git clone --depth=1 git://github.com/sass/libsass.git
 git clone --depth=1 git://github.com/sass/sassc.git
 
 export SASS_LIBSASS_PATH=/tmp/libsass
 
+# build the sassc binary
 cd sassc
 make
-sudo make install
+
+# isntall the sassc binary
+sudo install -t /usr/local/bin bin/sassc
