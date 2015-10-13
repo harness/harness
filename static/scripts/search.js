146d884f114a72ce421aeaca3a45c37db3d8e3d1
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-10-13 12:55:50 -0700

    provide more useful error messages when empty search results

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,4 +1,4 @@
-
+var repoExpr = /.+\/.+/;
 
 var remoteRepos = new Bloodhound({
     queryTokenizer: Bloodhound.tokenizers.whitespace,
@@ -27,6 +27,36 @@ $('.typeahead').typeahead({
 	display: "full_name",
 	source: reposWithDefaults,
 	templates: {
+		empty: function(obj) {
+			if (obj.query.match(repoExpr) !== null) {
+				return [
+					"<div>",
+						"<div class='not-indexed-message'>",
+							"<p>",
+								"No matches found for",
+								"<em>",
+								obj.query,
+								"</em>",
+							"</p>",
+							"<p>",
+								"This repository may not be indexed yet.",
+								"<a href='/"+obj.query+"'>",
+								"Click here",
+								"</a>",
+								"to visit this repository page directly.",
+							"</p>",
+						"</div>",
+					"</div>"
+				].join("\n");
+			}
+			return [
+				"<div>",
+					"<div class='no-matches-message'>",
+						"No matches found",
+					"</div>",
+				"</div>"
+			].join("\n");
+		},
 		suggestion: function(obj) {
 			return "<div><div><img src='"+obj.avatar_url+"' width='32px' height='32px' /></div><div>"+ obj.full_name +"</div></div>";
 		}
