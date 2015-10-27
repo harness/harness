284c99175ff7e1541c9a31735e5da0352f7d177b
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-10-27 13:44:57 -0700

    build_deploy should default to empty, no nil. just to be safe

--- store/migration/mysql/2.sql
+++ store/migration/mysql/2.sql
@@ -3,7 +3,8 @@
 ALTER TABLE repos  ADD COLUMN repo_scm     VARCHAR(25);
 ALTER TABLE builds ADD COLUMN build_deploy VARCHAR(500);
 
-UPDATE repos SET repo_scm = 'git';
+UPDATE repos  SET repo_scm = 'git';
+UPDATE builds SET build_deploy = '';
 
 -- +migrate Down
 
--- store/migration/sqlite3/2.sql
+++ store/migration/sqlite3/2.sql
@@ -3,7 +3,8 @@
 ALTER TABLE repos  ADD COLUMN repo_scm     TEXT;
 ALTER TABLE builds ADD COLUMN build_deploy TEXT;
 
-UPDATE repos SET repo_scm = 'git';
+UPDATE repos  SET repo_scm = 'git';
+UPDATE builds SET build_deploy = '';
 
 -- +migrate Down
 
