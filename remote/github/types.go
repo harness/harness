819a9504e3b42c8bf525176e6ea9afbdf7677a96
Brad Rydzewski <brad.rydzewski@gmail.com>
2015-10-26 17:29:29 -0700

    parses github deployment hooks for #1144 into build objects

--- remote/github/types.go
+++ remote/github/types.go
@@ -29,7 +29,39 @@ type pushHook struct {
 	Sender struct {
 		Login  string `json:"login"`
 		Avatar string `json:"avatar_url"`
-	}
+	} `json:"sender"`
+
+	Repo struct {
+		Owner struct {
+			Login string `json:"login"`
+			Name  string `json:"name"`
+		} `json:"owner"`
+
+		Name          string `json:"name"`
+		FullName      string `json:"full_name"`
+		Language      string `json:"language"`
+		Private       bool   `json:"private"`
+		HTMLURL       string `json:"html_url"`
+		CloneURL      string `json:"clone_url"`
+		DefaultBranch string `json:"default_branch"`
+	} `json:"repository"`
+}
+
+type deployHook struct {
+	Deployment struct {
+		ID   int64  `json:"id"`
+		Sha  string `json:"sha"`
+		Ref  string `json:"ref"`
+		Task string `json:"task"`
+		Env  string `json:"environment"`
+		Url  string `json:"url"`
+		Desc string `json:"description"`
+	} `json:"deployment"`
+
+	Sender struct {
+		Login  string `json:"login"`
+		Avatar string `json:"avatar_url"`
+	} `json:"sender"`
 
 	Repo struct {
 		Owner struct {
