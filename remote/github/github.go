02e76553feb19597c046234d9a9f12df2599075e
Fabio Rapposelli <fabio@vmware.com>
2015-12-22 14:10:39 +0100

    Make GitHub status API context congruous
    Signed-off-by: Fabio Rapposelli <fabio@vmware.com>

--- remote/github/github.go
+++ remote/github/github.go
@@ -230,7 +230,7 @@ func (g *Github) Status(u *model.User, r *model.Repo, b *model.Build, link strin
 	status := getStatus(b.Status)
 	desc := getDesc(b.Status)
 	data := github.RepoStatus{
-		Context:     github.String("Drone"),
+		Context:     github.String("continuous-integration/drone"),
 		State:       github.String(status),
 		Description: github.String(desc),
 		TargetURL:   github.String(link),
