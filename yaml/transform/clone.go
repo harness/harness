18fdffd78718268a15236e1584490e52d004e58a
Brad Rydzewski <brad.rydzewski@gmail.com>
2016-06-03 11:45:06 -0700

    default git if not set

--- yaml/transform/clone.go
+++ yaml/transform/clone.go
@@ -6,13 +6,17 @@ const clone = "clone"
 
 // Clone transforms the Yaml to include a clone step.
 func Clone(c *yaml.Config, plugin string) error {
-	for _, p := range c.Pipeline {
-		if p.Name == clone {
-			return nil
-		}
-	}
 	if plugin == "" {
 		plugin = "git"
+	}
+
+	for _, p := range c.Pipeline {
+		if p.Name == clone {
+			if p.Image == "" {
+				p.Image = plugin
+			}
+			return nil
+		}
 	}
 
 	s := &yaml.Container{
