ece6806da16ccd4fbb632e0159839159ae1babf6
Don <don.j.olmstead@gmail.com>
2016-07-03 11:36:54 -0700

    Add IndexOf and Length to Queue

--- queue/queue.go
+++ queue/queue.go
@@ -12,14 +12,15 @@ import (
 // exist in the queue.
 var ErrNotFound = errors.New("queue item not found")
 
+// Queue represents a worker queue.
 type Queue interface {
 	// Publish inserts work at the tail of this queue, waiting for
 	// space to become available if the queue is full.
 	Publish(*Work) error
 
-	// Remove removes the specified work item from this queue,
+	// Remove removes the specified job ID from this queue,
 	// if it is present.
-	Remove(*Work) error
+	Remove(int64) error
 
 	// PullClose retrieves and removes the head of this queue,
 	// waiting if necessary until work becomes available.
@@ -30,6 +31,13 @@ type Queue interface {
 	// CloseNotifier should be provided to clone the channel
 	// if the subscribing client terminates its connection.
 	PullClose(CloseNotifier) *Work
+
+	// IndexOf retrieves the current position within the queue of
+	// the job ID or -1 if the work is not found.
+	IndexOf(int64) int
+
+	// Length retrieves the length of the queue.
+	Length() int
 }
 
 // Publish inserts work at the tail of this queue, waiting for
@@ -38,10 +46,10 @@ func Publish(c context.Context, w *Work) error {
 	return FromContext(c).Publish(w)
 }
 
-// Remove removes the specified work item from this queue,
+// Remove removes the specified job ID from this queue,
 // if it is present.
-func Remove(c context.Context, w *Work) error {
-	return FromContext(c).Remove(w)
+func Remove(c context.Context, id int64) error {
+	return FromContext(c).Remove(id)
 }
 
 // Pull retrieves and removes the head of this queue,
@@ -56,6 +64,17 @@ func Pull(c context.Context) *Work {
 // if the subscribing client terminates its connection.
 func PullClose(c context.Context, cn CloseNotifier) *Work {
 	return FromContext(c).PullClose(cn)
+}
+
+// IndexOf retrieves the current position within the queue of
+// the job ID or -1 if the work is not found.
+func IndexOf(c context.Context, id int64) int {
+	return FromContext(c).IndexOf(id)
+}
+
+// Length retrieves the length of the queue.
+func Length(c context.Context) int {
+	return FromContext(c).Length()
 }
 
 // CloseNotifier defines a datastructure that is capable of notifying
