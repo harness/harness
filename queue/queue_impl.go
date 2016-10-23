a48294311fb5c7e0079c101196205cbe3d96e2d5
磊  王 <leiwang@leideMacBook-Pro.local>
2016-10-23 18:31:38 +0800

    add label support

--- queue/queue_impl.go
+++ queue/queue_impl.go
@@ -1,12 +1,15 @@
 package queue
 
-import "sync"
+import (
+	"reflect"
+	"sync"
+)
 
 type queue struct {
 	sync.Mutex
 
 	items map[*Work]struct{}
-	itemc chan *Work
+	itemc map[string]chan *Work
 }
 
 func New() Queue {
@@ -16,15 +19,21 @@ func New() Queue {
 func newQueue() *queue {
 	return &queue{
 		items: make(map[*Work]struct{}),
-		itemc: make(chan *Work, 999),
+		itemc: make(map[string]chan *Work),
 	}
 }
 
 func (q *queue) Publish(work *Work) error {
 	q.Lock()
+	if work.Label == "" {
+		work.Label = DefaultLabel
+	}
+	if _, ok := q.itemc[work.Label]; !ok {
+		q.itemc[work.Label] = make(chan *Work, 999)
+	}
 	q.items[work] = struct{}{}
 	q.Unlock()
-	q.itemc <- work
+	q.itemc[work.Label] <- work
 	return nil
 }
 
@@ -43,7 +52,7 @@ func (q *queue) Remove(work *Work) error {
 drain:
 	for {
 		select {
-		case item := <-q.itemc:
+		case item := <-q.itemc[work.Label]:
 			items = append(items, item)
 		default:
 			break drain
@@ -57,29 +66,44 @@ drain:
 			delete(q.items, work)
 			continue
 		}
-		q.itemc <- item
+		q.itemc[work.Label] <- item
 	}
 	return nil
 }
 
-func (q *queue) Pull() *Work {
-	work := <-q.itemc
-	q.Lock()
-	delete(q.items, work)
-	q.Unlock()
+func (q *queue) PullWithLabels(labels []string) *Work {
+	cases := make([]reflect.SelectCase, 0)
+	for _, label := range labels {
+		if _, ok := q.itemc[label]; ok {
+			cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(q.itemc[label])})
+		}
+	}
+	_, value, _ := reflect.Select(cases)
+	work := value.Interface().(*Work)
 	return work
 }
 
+func (q *queue) Pull() *Work {
+	return q.PullWithLabels([]string{DefaultLabel})
+}
+
 func (q *queue) PullClose(cn CloseNotifier) *Work {
-	for {
-		select {
-		case <-cn.CloseNotify():
-			return nil
-		case work := <-q.itemc:
-			q.Lock()
-			delete(q.items, work)
-			q.Unlock()
-			return work
+	return q.PullCloseWithLabels([]string{DefaultLabel}, cn)
+}
+
+func (q *queue) PullCloseWithLabels(labels []string, cn CloseNotifier) *Work {
+	cases := make([]reflect.SelectCase, 0)
+	for _, label := range labels {
+		if _, ok := q.itemc[label]; ok {
+			cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(q.itemc[label])})
 		}
+	}
+	cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(cn.CloseNotify())})
+	sel, value, _ := reflect.Select(cases)
+	if sel == len(cases)-1 {
+		return nil
+	} else {
+		work := value.Interface().(*Work)
+		return work
 	}
 }
