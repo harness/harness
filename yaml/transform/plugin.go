e4740667c063178c648d2f2a288e2069ca06d8c5
Brad Rydzewski <brad.rydzewski@gmail.com>
2017-01-20 18:06:08 +0700

    use when.local=false to disable plugin steps locally

--- yaml/transform/plugin.go
+++ yaml/transform/plugin.go
@@ -7,17 +7,13 @@ import "github.com/drone/drone/yaml"
 // locally on your own computer.
 func PluginDisable(conf *yaml.Config, local bool) error {
 	for _, container := range conf.Pipeline {
-		if len(container.Commands) != 0 || container.Detached { // skip build steps
-			continue
+		if local && !container.Constraints.Local.Bool() {
+			container.Disabled = true
 		}
 
 		if isClone(container) {
 			container.Disabled = true
 			continue
-		}
-
-		if local && container.Constraints.Runtime.Match("cli") {
-			container.Disabled = true
 		}
 	}
 	return nil
