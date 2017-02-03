bf3ddf5611ba3547fe1b3d5ddcad263ea7874078
Robert Stettner <robert.stettner@gmail.com>
2017-02-03 15:47:21 +0000

    Added build event and deploy to hooks

--- server/hook.go
+++ server/hook.go
@@ -243,6 +243,14 @@ func PostHook(c *gin.Context) {
 			stomp.WithHeaders(
 				yaml.ParseLabel(raw),
 			),
+			stomp.WithHeader(
+				"event",
+				build.Event,
+			),
+			stomp.WithHeader(
+				"deploy_to",
+				build.Deploy,
+			),
 		)
 	}
 
