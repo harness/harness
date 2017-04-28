f65f8a2eea5a380af55daf26f8868abd6429374d
Jesse Donat <jdonat@myon.com>
2017-04-28 10:03:06 -0500

    Adds the current first “overall” CCProject

--- model/cc.go
+++ model/cc.go
@@ -24,8 +24,10 @@ type CCProject struct {
 func NewCC(r *Repo, bs []*Build, link string) *CCProjects {
 	projs := &CCProjects{Projects: []*CCProject{}}
 
-	branches := []string{}
+	pz := NewCCProject(r, bs[0], r.FullName, link)
+	projs.Projects = append(projs.Projects, pz)
 
+	branches := []string{}
 BuildLoop:
 	for _, b := range bs {
 		for _, br := range branches {
@@ -34,35 +36,39 @@ BuildLoop:
 			}
 		}
 
-		proj := &CCProject{
-			Name:            r.FullName + " " + b.Branch,
-			WebURL:          link,
-			Activity:        "Building",
-			LastBuildStatus: "Unknown",
-			LastBuildLabel:  "Unknown",
-		}
+		p := NewCCProject(r, b, r.FullName+" "+b.Branch, link)
 
-		// if the build is not currently running then
-		// we can return the latest build status.
-		if b.Status != StatusPending &&
-			b.Status != StatusRunning {
-			proj.Activity = "Sleeping"
-			proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)
-			proj.LastBuildLabel = strconv.Itoa(b.Number)
-		}
-
-		// ensure the last build Status accepts a valid
-		// ccmenu enumeration
-		switch b.Status {
-		case StatusError, StatusKilled:
-			proj.LastBuildStatus = "Exception"
-		case StatusSuccess:
-			proj.LastBuildStatus = "Success"
-		case StatusFailure:
-			proj.LastBuildStatus = "Failure"
-		}
-
+		projs.Projects = append(projs.Projects, p)
 		branches = append(branches, b.Branch)
 	}
 	return projs
+}
+
+func NewCCProject(r *Repo, b *Build, name, link string) *CCProject {
+	proj := &CCProject{
+		Name:            name,
+		WebURL:          link,
+		Activity:        "Building",
+		LastBuildStatus: "Unknown",
+		LastBuildLabel:  "Unknown",
+	}
+	// if the build is not currently running then
+	// we can return the latest build status.
+	if b.Status != StatusPending &&
+		b.Status != StatusRunning {
+		proj.Activity = "Sleeping"
+		proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)
+		proj.LastBuildLabel = strconv.Itoa(b.Number)
+	}
+	// ensure the last build Status accepts a valid
+	// ccmenu enumeration
+	switch b.Status {
+	case StatusError, StatusKilled:
+		proj.LastBuildStatus = "Exception"
+	case StatusSuccess:
+		proj.LastBuildStatus = "Success"
+	case StatusFailure:
+		proj.LastBuildStatus = "Failure"
+	}
+	return proj
 }
