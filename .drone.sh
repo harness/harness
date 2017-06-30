e3808bc0d8579af7151c1d2e21d89a479156c560
Brad Rydzewski <brad.rydzewski@gmail.com>
2017-06-29 20:38:43 -0400

    build agent with CGO disabled [ci skip]

--- .drone.sh
+++ .drone.sh
@@ -23,4 +23,4 @@ git clone git@github.com:drone/drone-enterprise.git extras
 
 # build a static binary with the build number and extra features.
 go build -ldflags '-extldflags "-static" -X github.com/drone/drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER} -o release/drone-server github.com/drone/drone/extras/cmd/drone-server
-go build -ldflags '-extldflags "-static" -X github.com/drone/drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER} -o release/drone-agent  github.com/drone/drone/cmd/drone-agent
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags '-X github.com/drone/drone/version.VersionDev=build.'${DRONE_BUILD_NUMBER} -o release/drone-agent github.com/drone/drone/cmd/drone-agent
