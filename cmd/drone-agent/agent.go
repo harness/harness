7f49e696072f66ab110ef644196b2e3444fa1ac3
Thomas Boerger <thomas@webhippie.de>
2018-03-21 14:02:17 +0100

    Just fixed format with go fmt ./...

--- cmd/drone-agent/agent.go
+++ cmd/drone-agent/agent.go
@@ -1,11 +1,11 @@
 // Copyright 2018 Drone.IO Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -91,7 +91,7 @@ func loop(c *cli.Context) error {
 			password: c.String("password"),
 		}),
 		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time: c.Duration("keepalive-time"),
+			Time:    c.Duration("keepalive-time"),
 			Timeout: c.Duration("keepalive-timeout"),
 		}),
 	)
