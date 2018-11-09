360708e93de1ee1c7aff829c6b832b9e2b5a5f5f
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-11-08 22:42:32 -0800

    optimize postgres query

--- store/datastore/sql/postgres/files/feed.sql
+++ store/datastore/sql/postgres/files/feed.sql
@@ -1,34 +1,14 @@
 -- name: feed-latest-build
 
-SELECT
- repo_owner
-,repo_name
-,repo_full_name
-,build_number
-,build_event
-,build_status
-,build_created
-,build_started
-,build_finished
-,build_commit
-,build_branch
-,build_ref
-,build_refspec
-,build_remote
-,build_title
-,build_message
-,build_author
-,build_email
-,build_avatar
-FROM repos LEFT OUTER JOIN builds ON build_id = (
-	SELECT build_id FROM builds
-	WHERE builds.build_repo_id = repos.repo_id
-	ORDER BY build_id DESC
-	LIMIT 1
-)
+SELECT repo_owner, repo_name, repo_full_name, b.*
+FROM repos LEFT OUTER JOIN (
+  SELECT DISTINCT ON (build_repo_id) build_repo_id, build_id, build_number, build_event, build_status, build_created, build_started, build_finished, build_commit, build_branch, build_ref, build_refspec, build_remote, build_title, build_message, build_author, build_email, build_avatar
+    FROM builds
+    ORDER BY build_repo_id, build_id DESC
+) b ON b.build_repo_id = repos.repo_id
 INNER JOIN perms ON perms.perm_repo_id = repos.repo_id
 WHERE perms.perm_user_id = $1
-  AND repos.repo_active = true
+  AND repos.repo_active = TRUE
 ORDER BY repo_full_name ASC;
 
 -- name: feed
