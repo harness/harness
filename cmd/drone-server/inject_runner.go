73c4f6bbcf158c6e44e6668999ef22bdc7c4ba29
Tait Clarridge <tait@clarridge.ca>
2019-02-21 17:23:42 -0500

    Fix check to avoid running agent on server
    - This was causing the agent to spin up on the server even though
      it was not enabled

--- cmd/drone-server/inject_runner.go
+++ cmd/drone-server/inject_runner.go
@@ -40,7 +40,7 @@ func provideRunner(
 ) *runner.Runner {
 	// the local runner is only created when the nomad or
 	// kubernetes scheduler are disabled
-	if config.Nomad.Enabled || config.Kube.Enabled || config.Agent.Enabled {
+	if config.Nomad.Enabled || config.Kube.Enabled || !config.Agent.Enabled {
 		return nil
 	}
 	engine, err := docker.NewEnv()
