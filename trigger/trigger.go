93e052a7132770e8bd9c20144b30842882346186
Lucian Jones <lucianm.jones@gmail.com>
2019-06-25 14:48:46 +1200

    process pull request action conditions in triggers

--- trigger/trigger.go
+++ trigger/trigger.go
@@ -258,6 +258,9 @@ func (t *triggerer) Trigger(ctx context.Context, repo *core.Repository, base *co
 		} else if skipEvent(pipeline, base.Event) {
 			logger = logger.WithField("pipeline", pipeline.Name)
 			logger.Infoln("trigger: skipping pipeline, does not match event")
+		} else if skipAction(pipeline, base.Action) {
+			logger = logger.WithField("pipeline", pipeline.Name).WithField("action", base.Action)
+			logger.Infoln("trigger: skipping pipeline, does not match action")
 		} else if skipRef(pipeline, base.Ref) {
 			logger = logger.WithField("pipeline", pipeline.Name)
 			logger.Infoln("trigger: skipping pipeline, does not match ref")
