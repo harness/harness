bcdd4bf0245c82c060407b3b24b9b87301d15ac1
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-08-06 23:16:21 -0700

    add .exe extension to windows binaries

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,2 @@
 $env:CGO_ENABLED="0"
-go build -o release/windows/amd64/drone-agent github.com/drone/drone/cmd/drone-agent
+go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent
