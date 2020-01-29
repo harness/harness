adef00064edf2b2abc06c98690e9bcb5240f5b0e
Brad Rydzewski <brad.rydzewski@gmail.com>
2020-01-29 08:57:37 -0800

    update scm mocks

--- mock/mockscm/mock_gen.go
+++ mock/mockscm/mock_gen.go
@@ -80,6 +80,22 @@ func (mr *MockContentServiceMockRecorder) Find(arg0, arg1, arg2, arg3 interface{
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Find", reflect.TypeOf((*MockContentService)(nil).Find), arg0, arg1, arg2, arg3)
 }
 
+// List mocks base method
+func (m *MockContentService) List(arg0 context.Context, arg1, arg2, arg3 string, arg4 scm.ListOptions) ([]*scm.ContentInfo, *scm.Response, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "List", arg0, arg1, arg2, arg3, arg4)
+	ret0, _ := ret[0].([]*scm.ContentInfo)
+	ret1, _ := ret[1].(*scm.Response)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// List indicates an expected call of List
+func (mr *MockContentServiceMockRecorder) List(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "List", reflect.TypeOf((*MockContentService)(nil).List), arg0, arg1, arg2, arg3, arg4)
+}
+
 // Update mocks base method
 func (m *MockContentService) Update(arg0 context.Context, arg1, arg2 string, arg3 *scm.ContentParams) (*scm.Response, error) {
 	m.ctrl.T.Helper()
@@ -116,6 +132,22 @@ func NewMockGitService(ctrl *gomock.Controller) *MockGitService {
 // EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockGitService) EXPECT() *MockGitServiceMockRecorder {
 	return m.recorder
+}
+
+// CompareChanges mocks base method
+func (m *MockGitService) CompareChanges(arg0 context.Context, arg1, arg2, arg3 string, arg4 scm.ListOptions) ([]*scm.Change, *scm.Response, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CompareChanges", arg0, arg1, arg2, arg3, arg4)
+	ret0, _ := ret[0].([]*scm.Change)
+	ret1, _ := ret[1].(*scm.Response)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// CompareChanges indicates an expected call of CompareChanges
+func (mr *MockGitServiceMockRecorder) CompareChanges(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CompareChanges", reflect.TypeOf((*MockGitService)(nil).CompareChanges), arg0, arg1, arg2, arg3, arg4)
 }
 
 // FindBranch mocks base method
