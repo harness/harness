63b9a70bf9479fc7166e4171f2742ac269fd7680
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2020-04-25 17:40:31 +0200

    Add TLS_FALLBACK_SCSV to the cipher list

--- server/server.go
+++ server/server.go
@@ -67,6 +67,7 @@ var (
 		"AES_128_GCM_SHA256":                  tls.TLS_AES_128_GCM_SHA256,
 		"AES_256_GCM_SHA384":                  tls.TLS_AES_256_GCM_SHA384,
 		"CHACHA20_POLY1305_SHA256":            tls.TLS_CHACHA20_POLY1305_SHA256,
+		"FALLBACK_SCSV":                       tls.TLS_FALLBACK_SCSV,
 	}
 )
 
