e9ee2fa5caff606cba23ab7f6b4944b166a54953
dakele <wmxy123@yeah.net>
2020-06-21 23:20:26 +0800

    feat: delete scm webhook when disable repo.

--- handler/api/repos/disable.go
+++ handler/api/repos/disable.go
@@ -15,6 +15,7 @@
 package repos
 
 import (
+	"github.com/drone/drone/handler/api/request"
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -27,6 +28,7 @@ import (
 // HandleDisable returns an http.HandlerFunc that processes http
 // requests to disable a repository in the system.
 func HandleDisable(
+	hooks core.HookService,
 	repos core.RepositoryStore,
 	sender core.WebhookSender,
 ) http.HandlerFunc {
@@ -36,6 +38,7 @@ func HandleDisable(
 			name  = chi.URLParam(r, "name")
 		)
 
+		user, _ := request.UserFrom(r.Context())
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
@@ -47,6 +50,18 @@ func HandleDisable(
 			return
 		}
 		repo.Active = false
+
+		err = hooks.Delete(r.Context(), user, repo)
+		if err != nil {
+			render.InternalError(w, err)
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", owner).
+				WithField("name", name).
+				Debugln("api: cannot delete hook")
+			return
+		}
+
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
 			render.InternalError(w, err)
