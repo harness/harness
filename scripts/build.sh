20623f81c4b3db2801bbd55686e8e90d8c8882ca
guessi <guessi@gmail.com>
2020-09-24 14:46:32 +0800

    Refine .drone.yml pipeline design
    Fixed the following problems:
    
    - Golang version in `.drone.jsonnet` was pointed to [1.11](https://github.com/drone/drone/blob/v1.7.0/.drone.jsonnet#L4), but [1.13.0](https://github.com/drone/drone/blob/v1.7.0/.drone.yml#L12) in `.drone.yml`
    
    - Multiple fileds not aligned in `.drone.yml`, `.drone.jsonnet`.
    
    **Updated**
    
    - May 10, 2020 - remove section of `drone-agent` and `drone-controller`
    - Sep 24, 2020 - rebase v1.9.1
    - Sep 24, 2020 - `.drone.yml` and `.drone.jsonnet` was removed, ref: https://github.com/drone/drone/commit/0e4af8d35b9fd3ecd1302a9a96d3efcbe64414dc

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,4 @@ echo "building docker images for ${GOOS}/${GOARCH} ..."
 REPO="github.com/drone/drone"
 
 # compile the server using the cgo
-go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
-
-# compile the runners with gcc disabled
-export CGO_ENABLED=0
-go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
-go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
+go build -ldflags "-extldflags \"-static\"" -o release/${GOOS}/${GOARCH}/drone-server ${REPO}/cmd/drone-server
