5f8eff4e80609efa02ab11a8abd06933de78f521
Alessandro De Maria <a.demaria@synthace.com>
2020-10-27 20:39:19 +0000

    builds nolimit drone

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ echo "building docker images for ${GOOS}/${GOARCH} ..."
 REPO="github.com/drone/drone"
 
 # compile the server using the cgo
-go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
+go build -tags "nolimit" -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
 # compile the runners with gcc disabled
 export CGO_ENABLED=0
