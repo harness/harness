9b89e0b4b74d459d6b838cefde6af8609a8d8cf3
bakito <github@bakito.ch>
2020-12-30 17:23:21 +0100

    stash needs to get ref via branch, cant via heads same as bitbucket

--- service/commit/commit.go
+++ service/commit/commit.go
@@ -78,7 +78,7 @@ func (s *service) FindRef(ctx context.Context, user *core.User, repo, ref string
 	})
 
 	switch s.client.Driver {
-	case scm.DriverBitbucket:
+	case scm.DriverBitbucket, scm.DriverStash:
 		ref = scm.TrimRef(ref)
 		branch, _, err := s.client.Git.FindBranch(ctx, repo, ref) // wont work for a Tag
 		if err != nil {
