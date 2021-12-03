b46e6459f32dc42410e6b7542b5a602f31f08afb
Eoin McAfee <83226740+eoinmcafee00@users.noreply.github.com>
2021-12-03 14:52:43 +0000

    update from drone to harness

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 echo "building docker images for ${GOOS}/${GOARCH} ..."
 
-REPO="github.com/drone/drone"
+REPO="github.com/harness/drone"
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
