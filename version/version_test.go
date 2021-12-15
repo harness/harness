abe1824354d8db9ea53806ad1dbac5f6d9d4d795
Dan Wilson <daniel.wilson@harness.io>
2021-12-15 13:51:39 +0000

    release v2.7.0
    update changelog

--- version/version_test.go
+++ version/version_test.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
+//go:build !oss
 // +build !oss
 
 package version
@@ -9,7 +10,7 @@ package version
 import "testing"
 
 func TestVersion(t *testing.T) {
-	if got, want := Version.String(), "2.6.0"; got != want {
+	if got, want := Version.String(), "2.7.0"; got != want {
 		t.Errorf("Want version %s, got %s", want, got)
 	}
 }
