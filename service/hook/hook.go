b1370969a6d7b79ffa4efd72503cc58f19b49602
haierinterx <37792460+haierinterx@users.noreply.github.com>
2022-05-13 14:07:37 +0800

    disable other events exclude tag in hook

--- service/hook/hook.go
+++ service/hook/hook.go
@@ -48,10 +48,10 @@ func (s *service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 		Target: s.addr + "/hook",
 		Secret: repo.Signer,
 		Events: scm.HookEvents{
-			Branch:      true,
-			Deployment:  true,
-			PullRequest: true,
-			Push:        true,
+// 			Branch:      true,
+// 			Deployment:  true,
+// 			PullRequest: true,
+// 			Push:        true,
 			Tag:         true,
 		},
 	}
