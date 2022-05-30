9b3906efa202045156ccde3ce38bb1a0037608df
kit101 <qkssk1711@163.com>
2022-05-30 10:11:57 +0800

    implement MockUserService.ListEmail

--- mock/mockscm/mock_gen.go
+++ mock/mockscm/mock_gen.go
@@ -797,3 +797,19 @@ func (mr *MockUserServiceMockRecorder) FindLogin(arg0, arg1 interface{}) *gomock
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindLogin", reflect.TypeOf((*MockUserService)(nil).FindLogin), arg0, arg1)
 }
+
+// ListEmail mocks base method
+func (m *MockUserService) ListEmail(arg0 context.Context, arg1 scm.ListOptions) ([]*scm.Email, *scm.Response, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "ListEmail", arg0, arg1)
+	ret0, _ := ret[0].([]*scm.Email)
+	ret1, _ := ret[1].(*scm.Response)
+	ret2, _ := ret[2].(error)
+	return ret0, ret1, ret2
+}
+
+// ListEmail indicates an expected call of ListEmail.
+func (mr *MockUserServiceMockRecorder) ListEmail(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListEmail", reflect.TypeOf((*MockUserService)(nil).ListEmail), arg0, arg1)
+}
