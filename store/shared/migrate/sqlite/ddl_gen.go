d7e34af1ded5d3b7f72083ab1f86448a063e4ced
Eoin McAfee <83226740+eoinmcafee00@users.noreply.github.com>
2022-06-15 09:11:28 +0100

    (bug) - fix original template scripts & remove amend scripts (#3229)
    * removing amend template scripts and fixing original scripts due to Brads feedback

--- store/shared/migrate/sqlite/ddl_gen.go
+++ store/shared/migrate/sqlite/ddl_gen.go
@@ -161,6 +161,10 @@ var migrations = []struct {
 		stmt: createTableTemplates,
 	},
 	{
+		name: "create-index-template-namespace",
+		stmt: createIndexTemplateNamespace,
+	},
+	{
 		name: "alter-table-steps-add-column-step-depends-on",
 		stmt: alterTableStepsAddColumnStepDependsOn,
 	},
@@ -195,10 +199,6 @@ var migrations = []struct {
 	{
 		name: "create-new-table-cards",
 		stmt: createNewTableCards,
-	},
-	{
-		name: "amend-templates-table",
-		stmt: amendTemplatesTable,
 	},
 }
 
@@ -698,14 +698,16 @@ CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
 var createTableTemplates = `
 CREATE TABLE IF NOT EXISTS templates (
      template_id      INTEGER PRIMARY KEY AUTOINCREMENT
-    ,template_name    TEXT UNIQUE
+    ,template_name    TEXT
     ,template_namespace TEXT COLLATE NOCASE
     ,template_data    BLOB
     ,template_created INTEGER
     ,template_updated INTEGER
-,UNIQUE(template_name, template_namespace)
+    ,UNIQUE(template_name COLLATE NOCASE, template_namespace COLLATE NOCASE)
 );
+`
 
+var createIndexTemplateNamespace = `
 CREATE INDEX IF NOT EXISTS ix_template_namespace ON templates (template_namespace);
 `
 
@@ -768,37 +770,4 @@ CREATE TABLE IF NOT EXISTS cards
     card_data BLOB,
     FOREIGN KEY (card_id) REFERENCES steps (step_id) ON DELETE CASCADE
 );
-`
-
-//
-// 019_amend_table_templates.sql
-//
-
-var amendTemplatesTable = `
-PRAGMA foreign_keys=off;
-
-BEGIN TRANSACTION;
-
-ALTER TABLE templates RENAME TO _templates_old;
-
-CREATE TABLE IF NOT EXISTS templates (
-     template_id      INTEGER PRIMARY KEY AUTOINCREMENT
-    ,template_name    TEXT
-    ,template_namespace TEXT COLLATE NOCASE
-    ,template_data    BLOB
-    ,template_created INTEGER
-    ,template_updated INTEGER
-    ,UNIQUE(template_name, template_namespace)
-);
-
-INSERT INTO templates (template_id, template_name, template_namespace, template_data, template_created, template_updated)
-SELECT template_id, template_name, template_namespace, template_data, template_created, template_updated
-FROM _templates_old;
-
-COMMIT;
-
-CREATE INDEX IF NOT EXISTS ix_template_namespace ON templates (template_namespace);
-DROP TABLE _templates_old;
-
-PRAGMA foreign_keys=on;
 `
--- store/shared/migrate/sqlite/files/015_create_template_tables.sql
+++ store/shared/migrate/sqlite/files/015_create_template_tables.sql
@@ -2,12 +2,14 @@
 
 CREATE TABLE IF NOT EXISTS templates (
      template_id      INTEGER PRIMARY KEY AUTOINCREMENT
-    ,template_name    TEXT UNIQUE
+    ,template_name    TEXT
     ,template_namespace TEXT COLLATE NOCASE
     ,template_data    BLOB
     ,template_created INTEGER
     ,template_updated INTEGER
-,UNIQUE(template_name, template_namespace)
+    ,UNIQUE(template_name COLLATE NOCASE, template_namespace COLLATE NOCASE)
 );
+
+-- name: create-index-template-namespace
 
 CREATE INDEX IF NOT EXISTS ix_template_namespace ON templates (template_namespace);
