1a89aaef348d05d3d08a42e0e589a97a3c01a137
soulteary <soulteary@gmail.com>
2022-07-03 19:05:08 +0800

    fix: fix SQLite warning when server building

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 echo "building docker images for ${GOOS}/${GOARCH} ..."
 
 REPO="github.com/drone/drone"
+CGO_CFLAGS="-g -O2 -Wno-return-local-addr"
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
