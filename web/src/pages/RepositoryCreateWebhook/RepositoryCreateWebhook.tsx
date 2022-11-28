b8cc24c491e4f5d4dd2ee5c5a55231564e87af5b
Sahithi Kolichala <sahithi.kolichala@harness.io>
2022-11-28 14:48:30 -0800

    feat: [SCM-42]: added route for createwebhook  (#93)
    * feat: added route for createwebhook
    
    * feat: added createwebhook route

---
+++ web/src/pages/RepositoryCreateWebhook/RepositoryCreateWebhook.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, PageBody } from '@harness/uicore'
+
+export default function RepositoryCreateWebhook() {
+  return (
+    <Container>
+      <PageBody>This is wEbhook page</PageBody>
+    </Container>
+  )
+}
--- web/src/pages/RepositorySettings/RepositorySettings.tsx
+++ web/src/pages/RepositorySettings/RepositorySettings.tsx
@@ -1,16 +1,38 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+
 import { PageBody, Button, Intent, Container, PageHeader } from '@harness/uicore'
 import { useGetRepositoryMetadata } from 'hooks/useGetRepositoryMetadata'
 import { useStrings } from 'framework/strings'
+import { useAppContext } from 'AppContext'
+import { CodeIcon } from 'utils/GitUtils'
 
 import { RepositorySettingsHeader } from './RepositorySettingsHeader/RepositorySettingsHeader'
 
 import emptyStateImage from './empty-state.svg'
+
 import css from './RepositorySettings.module.scss'
 
 export default function RepositorySettings() {
   const { repoMetadata, error, loading } = useGetRepositoryMetadata()
-  const NewWebHookButton = <Button type="submit" text={'Create Webhook'} intent={Intent.PRIMARY} />
+  const { routes } = useAppContext()
+  const history = useHistory()
+
+  const NewWebHookButton = (
+    <Button
+      type="button"
+      text={'Create Webhook'}
+      intent={Intent.PRIMARY}
+      icon={CodeIcon.Add}
+      onClick={() => {
+        history.push(
+          routes.toCODECreateWebhook({
+            repoPath: repoMetadata?.path as string
+          })
+        )
+      }}
+    />
+  )
   const { getString } = useStrings()
   return (
     <Container className={css.main}>
