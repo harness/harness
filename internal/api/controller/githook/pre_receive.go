fe00db23f980768517f450b8a6b6e841c24e4695
Hitesh Aringa <hitesh.aringa@harness.io>
2023-01-12 09:59:09 -0800

    [SCM-35]: default branch delete protection (#183)
    * [SCM-35]: default branch delete protection

--- internal/api/controller/githook/pre_receive.go
+++ internal/api/controller/githook/pre_receive.go
@@ -8,8 +8,12 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/harness/gitness/internal/api/usererror"
 	"github.com/harness/gitness/internal/auth"
+	"github.com/harness/gitness/types"
 	"github.com/harness/gitness/types/enum"
+
+	"github.com/gotidy/ptr"
 )
 
 // PreReceiveInput represents the input of the pre-receive git hook.
@@ -29,12 +33,30 @@ func (c *Controller) PreReceive(
 		return nil, fmt.Errorf("input is nil")
 	}
 
-	_, err := c.getRepoCheckAccess(ctx, session, in.RepoID, enum.PermissionRepoEdit)
+	repo, err := c.getRepoCheckAccess(ctx, session, in.RepoID, enum.PermissionRepoEdit)
 	if err != nil {
 		return nil, err
+	}
+
+	branchOutput := c.blockDefaultBranchDeletion(repo, in)
+	if branchOutput != nil {
+		return branchOutput, nil
 	}
 
 	// TODO: Branch Protection, Block non-brach/tag refs (?), ...
 
 	return &ServerHookOutput{}, nil
+}
+
+func (c *Controller) blockDefaultBranchDeletion(repo *types.Repository, in *PreReceiveInput) *ServerHookOutput {
+	repoDefaultBranchRef := gitReferenceNamePrefixBranch + repo.DefaultBranch
+
+	for _, refUpdate := range in.RefUpdates {
+		if refUpdate.New == types.NilSHA && refUpdate.Ref == repoDefaultBranchRef {
+			return &ServerHookOutput{
+				Error: ptr.String(usererror.ErrDefaultBranchCantBeDeleted.Error()),
+			}
+		}
+	}
+	return nil
 }
