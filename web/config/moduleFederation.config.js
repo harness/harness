04702def71481093721281289e98b90bd91ccb01
Tan Nhu <tan@harness.io>
2023-02-03 23:09:39 -0800

    Fix broken QA build due to lodash-es lib (#311)
    Co-authored-by: Tan Nhu <tnhu@users.noreply.github.com>

--- web/config/moduleFederation.config.js
+++ web/config/moduleFederation.config.js
@@ -5,20 +5,16 @@ const { pick, omit, mapValues } = require('lodash')
  * These packages must be stricly shared with exact versions
  */
 const ExactSharedPackages = [
-  'react',
   'react-dom',
+  'react',
   'react-router-dom',
   '@harness/use-modal',
   '@blueprintjs/core',
   '@blueprintjs/select',
   '@blueprintjs/datetime',
-  'restful-react'
-  // '@harness/monaco-yaml',
-  // 'monaco-editor',
-  // 'monaco-editor-core',
-  // 'monaco-languages',
-  // 'monaco-plugin-helpers',
-  // 'react-monaco-editor'
+  '@blueprintjs/icons',
+  'restful-react',
+  'lodash-es'
 ]
 
 /**
