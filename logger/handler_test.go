f984d4841f66d2efc785988531ffe652b62d1842
Colin Hoglund <colinhoglund@gmail.com>
2023-04-07 10:20:30 -0700

    Add authtype to logging middleware
    This is useful for determining the authentication method that is being used for server traffic.

--- logger/handler_test.go
+++ logger/handler_test.go
@@ -6,7 +6,10 @@
 
 package logger
 
-import "testing"
+import (
+	"net/http/httptest"
+	"testing"
+)
 
 func TestMiddleware(t *testing.T) {
 	t.Skip()
@@ -14,4 +17,22 @@ func TestMiddleware(t *testing.T) {
 
 func TestMiddleware_GenerateRequestID(t *testing.T) {
 	t.Skip()
+}
+
+func TestAuthType(t *testing.T) {
+	cookieRequest := httptest.NewRequest("GET", "/", nil)
+	if authType(cookieRequest) != "cookie" {
+		t.Error("authtype is not cookie")
+	}
+
+	headerRequest := httptest.NewRequest("GET", "/", nil)
+	headerRequest.Header.Add("Authorization", "test")
+	if authType(headerRequest) != "token" {
+		t.Error("authtype is not token")
+	}
+
+	formRequest := httptest.NewRequest("GET", "/?access_token=test", nil)
+	if authType(formRequest) != "token" {
+		t.Error("authtype is not token")
+	}
 }
