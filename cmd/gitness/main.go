4dde956e834a5d323e1cb83eafa1aecccd0df710
Atefeh Mohseni-Ejiyeh <atefeh.mohseni-ejiyeh@harness.io>
2023-07-05 16:29:02 +0000

    removed wrong build tags for gitness standalone (#175)

--- cmd/gitness/main.go
+++ cmd/gitness/main.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by the Polyform Free Trial License
 // that can be found in the LICENSE.md file for this repository.
 
-//go:build !pq
-// +build !pq
-
 package main
 
 import (
