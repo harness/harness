21d666b4307fa7875baebe26eb30c8ab2ec5d3de
Vistaar Juneja <vistaar.juneja@harness.io>
2023-08-04 16:08:12 +0100

    add list API

--- types/enum/scm.go
+++ types/enum/scm.go
@@ -1,6 +1,10 @@
+// Copyright 2022 Harness Inc. All rights reserved.
+// Use of this source code is governed by the Polyform Free Trial License
+// that can be found in the LICENSE.md file for this repository.
+
 package enum
 
-// ScmType defines the different types of principal types at harness.
+// ScmType defines the different SCM types supported for CI.
 type ScmType string
 
 func (ScmType) Enum() []interface{} { return toInterfaceSlice(scmTypes) }
