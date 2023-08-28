b15054ea767dbd8c96d03f095725f15431b437f3
Enver Bisevac <enver.bisevac@harness.io>
2023-08-28 11:22:17 +0200

    retry improved

--- lock/memory_test.go
+++ lock/memory_test.go
@@ -6,6 +6,7 @@ package lock
 
 import (
 	"context"
+	"errors"
 	"sync"
 	"testing"
 	"time"
@@ -31,8 +32,57 @@ func Test_inMemMutex_Lock(t *testing.T) {
 		}
 		if err := mx.Lock(context.Background()); err != nil {
 			t.Errorf("error from go routine while locking %s, err: %v", mx.Key(), err)
+			return
 		}
 		mx.Unlock(context.Background())
+	}()
+
+	mx, err := manager.NewMutex("key1")
+	if err != nil {
+		t.Errorf("mutex not created, err: %v", err)
+		return
+	}
+	if err := mx.Lock(context.Background()); err != nil {
+		t.Errorf("error while locking %v, err: %v", mx.Key(), err)
+	}
+	time.Sleep(1 * time.Second)
+	mx.Unlock(context.Background())
+	wg.Wait()
+}
+
+func Test_inMemMutex_MaxTries(t *testing.T) {
+	manager := NewInMemory(Config{
+		App:        "gitness",
+		Namespace:  "pullreq",
+		Expiry:     1 * time.Second,
+		Tries:      2,
+		RetryDelay: 300 * time.Millisecond,
+	})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(500 * time.Millisecond)
+		mx, err := manager.NewMutex("key1")
+		if err != nil {
+			t.Errorf("mutex not created, err: %v", err)
+			return
+		}
+
+		err = mx.Lock(context.Background())
+		if err == nil {
+			t.Errorf("error should be returned while locking %s instead of nil", mx.Key())
+			return
+		}
+		var errLock *Error
+		if !errors.As(err, &errLock) {
+			t.Errorf("expected error lock.Error, got: %v", err)
+			return
+		}
+		if errLock.Kind != MaxRetriesExceeded {
+			t.Errorf("expected lock.MaxRetriesExceeded, got: %v", err)
+			return
+		}
 	}()
 
 	mx, err := manager.NewMutex("key1")
