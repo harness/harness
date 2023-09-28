8fdcecf75c2a4346178d32ce0d2d3788c2a330a4
Vardan Bansal <vardan.bansal@harness.io>
2023-09-28 13:57:35 -0700

    minor bug fixes

--- web/src/components/MultiList/MultiList.tsx
+++ web/src/components/MultiList/MultiList.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { debounce, omit } from 'lodash'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { debounce, has, omit } from 'lodash'
 import { FormikContextType, connect } from 'formik'
 import { Layout, Text, FormInput, Button, ButtonVariation, ButtonSize, Container } from '@harnessio/uicore'
 import { FontVariation } from '@harnessio/design-system'
@@ -20,16 +20,25 @@ interface MultiListProps {
 
 export const MultiList = ({ name, label, readOnly, formik }: MultiListConnectedProps): JSX.Element => {
   const { getString } = useStrings()
-  const [rowCount, setRowCount] = useState<number>(0)
-  const [values, setValues] = useState<string[]>([])
+  const [valueMap, setValueMap] = useState<Map<string, string>>(new Map<string, string>([]))
+  /*
+  <field-name-1>: <field-value-1>,
+  <field-name-2>: <field-value-2>
+  */
+  const counter = useRef<number>(0)
 
   useEffect(() => {
+    const values = Array.from(valueMap.values())
     if (values.length > 0) {
       formik?.setFieldValue(name, values)
     } else {
       cleanupField()
     }
-  }, [values])
+  }, [valueMap])
+
+  const cleanupField = useCallback((): void => {
+    formik?.setValues(omit({ ...formik?.values }, name))
+  }, [formik?.values])
 
   const getFieldName = useCallback(
     (index: number): string => {
@@ -39,41 +48,58 @@ export const MultiList = ({ name, label, readOnly, formik }: MultiListConnectedP
   )
 
   const handleAddRowToList = useCallback((): void => {
-    setRowCount((existingCount: number) => existingCount + 1)
-  }, [])
-
-  const handleRemoveRowFromList = useCallback((removedRowIdx: number): void => {
-    setRowCount((existingCount: number) => existingCount - 1)
-    removeItemFromList(removedRowIdx)
-  }, [])
-
-  const handleAddItemToList = useCallback((event: React.FormEvent<HTMLInputElement>): void => {
-    const value = (event.target as HTMLInputElement).value
-    setValues((existingValues: string[]) => [...existingValues, value])
-  }, [])
-
-  const removeItemFromList = useCallback((removedRowIdx: number): void => {
-    setValues((existingValues: string[]) => {
-      const existingValuesCopy = [...existingValues]
-      if (removedRowIdx > -1) {
-        existingValuesCopy.splice(removedRowIdx, 1)
-        return existingValuesCopy
+    setValueMap((existingValueMap: Map<string, string>) => {
+      const rowKeyToAdd = getFieldName(counter.current)
+      if (!existingValueMap.has(rowKeyToAdd)) {
+        const existingValueMapClone = new Map(existingValueMap)
+        existingValueMapClone.set(rowKeyToAdd, '') /* Add key <field-name-1>, <field-name-2>, ... */
+        counter.current++ /* this counter always increases, even if a row is removed. This ensures no key collision in the existing value map. */
+        return existingValueMapClone
       }
-      return existingValues
+      return existingValueMap
     })
   }, [])
 
-  const cleanupField = useCallback((): void => {
-    formik?.setValues(omit({ ...formik?.values }, name))
-  }, [formik?.values])
+  const handleRemoveRowFromList = useCallback((removedRowKey: string): void => {
+    setValueMap((existingValueMap: Map<string, string>) => {
+      if (existingValueMap.has(removedRowKey)) {
+        const existingValueMapClone = new Map(existingValueMap)
+        existingValueMapClone.delete(removedRowKey)
+        return existingValueMapClone
+      }
+      return existingValueMap
+    })
+    /* remove <field-name-1>, <field-name-2>, ... from formik values, if exist */
+    if (removedRowKey && has(formik?.values, removedRowKey)) {
+      formik?.setValues(omit({ ...formik?.values }, removedRowKey))
+    }
+  }, [])
 
-  const debouncedAddItemToList = useCallback(debounce(handleAddItemToList, 500), [handleAddItemToList])
+  const handleAddItemToRow = useCallback((rowKey: string, insertedValue: string): void => {
+    setValueMap((existingValueMap: Map<string, string>) => {
+      if (existingValueMap.has(rowKey)) {
+        const existingValueMapClone = new Map(existingValueMap)
+        existingValueMapClone.set(rowKey, insertedValue)
+        return existingValueMapClone
+      }
+      return existingValueMap
+    })
+  }, [])
 
-  const renderRow = useCallback((rowIndex: number): React.ReactElement => {
+  const debouncedAddItemToList = useCallback(debounce(handleAddItemToRow, 500), [handleAddItemToRow])
+
+  const renderRow = useCallback((rowKey: string): React.ReactElement => {
     return (
       <Layout.Horizontal margin={{ bottom: 'none' }} flex={{ justifyContent: 'space-between', alignItems: 'center' }}>
         <Container width="90%">
-          <FormInput.Text name={getFieldName(rowIndex)} disabled={readOnly} onChange={debouncedAddItemToList} />
+          <FormInput.Text
+            name={rowKey}
+            disabled={readOnly}
+            onChange={event => {
+              const value = (event.target as HTMLInputElement).value
+              debouncedAddItemToList(rowKey, value)
+            }}
+          />
         </Container>
         <Icon
           name="code-delete"
@@ -82,26 +108,26 @@ export const MultiList = ({ name, label, readOnly, formik }: MultiListConnectedP
           className={css.deleteRowBtn}
           onClick={event => {
             event.preventDefault()
-            handleRemoveRowFromList(rowIndex)
+            handleRemoveRowFromList(rowKey)
           }}
         />
       </Layout.Horizontal>
     )
   }, [])
 
-  const renderRows = useCallback((numOfRows: number): React.ReactElement => {
+  const renderRows = useCallback((): React.ReactElement => {
     const rows: React.ReactElement[] = []
-    for (let idx = 0; idx < numOfRows; idx++) {
-      rows.push(renderRow(idx))
-    }
+    valueMap.forEach((_value: string, key: string) => {
+      rows.push(renderRow(key))
+    })
     return <Layout.Vertical>{rows}</Layout.Vertical>
-  }, [])
+  }, [valueMap])
 
   return (
     <Layout.Vertical spacing="small">
       <Layout.Vertical>
         <Text font={{ variation: FontVariation.FORM_LABEL }}>{label}</Text>
-        {rowCount > 0 && <Container padding={{ top: 'small' }}>{renderRows(rowCount)}</Container>}
+        {valueMap.size > 0 && <Container padding={{ top: 'small' }}>{renderRows()}</Container>}
       </Layout.Vertical>
       <Button
         text={getString('addLabel')}
