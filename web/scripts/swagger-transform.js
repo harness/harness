93b6f9cedfef6480cfa4eecf45465830f5cc7c03
Dharun Kumar <123951019+DharunKumar04@users.noreply.github.com>
2023-10-06 21:38:01 +0530

    fix-path-traversal-vulnerability (#3399)

--- web/scripts/swagger-transform.js
+++ web/scripts/swagger-transform.js
@@ -21,41 +21,43 @@ const yaml = require('js-yaml')
 const stringify = require('fast-json-stable-stringify')
 
 module.exports = inputSchema => {
-  const argv = process.argv.slice(2)
-  const config = argv[0]
+  // Use a hard-coded configuration to prevent user-defined input.
+  const config = 'code'
 
-  if (config) {
-    const overridesFile = path.join('src/services', config, 'overrides.yaml')
-    const transformFile = path.join('src/services', config, 'transform.js')
+  const overridesFile = path.join('src/services', config, 'overrides.yaml')
 
-    let paths = inputSchema.paths
+  let paths = inputSchema.paths
 
-    if (fs.existsSync(overridesFile)) {
-      const data = fs.readFileSync(overridesFile, 'utf8')
-      const { allowpaths, operationIdOverrides } = yaml.safeLoad(data)
+  if (fs.existsSync(overridesFile)) {
+    const data = fs.readFileSync(overridesFile, 'utf8')
+    const { allowpaths, operationIdOverrides } = yaml.safeLoad(data)
 
-      if (!allowpaths.includes('*')) {
-        paths = _.pick(paths, ...allowpaths)
+    if (!allowpaths.includes('*')) {
+      paths = _.pick(paths, ...allowpaths)
+    }
+
+    _.forIn(operationIdOverrides, (value, key) => {
+      const [path, method] = key.split('.')
+
+      if (path && method && _.has(paths, path) && _.has(paths[path], method)) {
+        _.set(paths, [path, method, 'operationId'], value)
       }
-
-      _.forIn(operationIdOverrides, (value, key) => {
-        const [path, method] = key.split('.')
-
-        if (path && method && _.has(paths, path) && _.has(paths[path], method)) {
-          _.set(paths, [path, method, 'operationId'], value)
-        }
-      })
-    }
-
-    inputSchema.paths = paths
-
-    if (fs.existsSync(transformFile)) {
-      const transform = require(path.resolve(process.cwd(), transformFile))
-
-      inputSchema = transform(inputSchema)
-    }
+    })
   }
 
-  // stringify and parse json to get a stable object
+  inputSchema.paths = paths
+
+  const transformFile = path.join('src/services', config, 'transform.js')
+
+  if (fs.existsSync(transformFile)) {
+    const transform = require(path.resolve(process.cwd(), transformFile))
+
+    inputSchema = transform(inputSchema)
+  } else {
+    console.error('Transform file not found for the specified config.')
+    process.exit(1)
+  }
+
+  // stringify and parse JSON to get a stable object
   return JSON.parse(stringify(inputSchema))
 }
