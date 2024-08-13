be471c2151132a335e22968d91b143ae8b87617f
Dhruv Dhruv <dhruv@harness.io>
2024-08-13 06:52:16 +0000

    fix: [CDE-232]: Dropping FK constraint and correcting order of dropping tables. (#2490)
    * fix: [CDE-232]: Dropping FK constraint and correcting order of dropping tables.

--- app/store/database/migrate/postgres/0052_create_cde_tables.down.sql
+++ app/store/database/migrate/postgres/0052_create_cde_tables.down.sql
@@ -1,7 +1,10 @@
-DROP TABLE infra_provider_configs;
-DROP TABLE infra_provider_templates;
-DROP TABLE infra_provider_resources;
-DROP TABLE gitspace_configs;
+ALTER TABLE infra_provisioned
+    DROP CONSTRAINT fk_iprov_gitspace_id;
+
+DROP TABLE gitspace_events;
 DROP TABLE gitspaces;
 DROP TABLE infra_provisioned;
-DROP TABLE gitspace_events;
+DROP TABLE gitspace_configs;
+DROP TABLE infra_provider_resources;
+DROP TABLE infra_provider_templates;
+DROP TABLE infra_provider_configs;
