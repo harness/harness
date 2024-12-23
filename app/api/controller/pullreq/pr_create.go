9d4fc2c796b7a5bf100541db7a4d1c712293600f
Darko Draskovic <darko.draskovic@harness.io>
2024-12-23 14:48:52 +0000

    feat: [CODE-2975]: Add reviewers creation to pull req creation (#3182)
    * Put pull req creation in tx
    * Fix auth error handling and struct/json key/field name
    * Merge remote-tracking branch 'origin/main' into dd/pr-create-reviewers
    * Move create reviewers functionality into a helper func
    * Add reviewers creation to pull req creation

--- app/api/controller/pullreq/pr_create.go
+++ app/api/controller/pullreq/pr_create.go
@@ -20,10 +20,13 @@ import (
 	"strings"
 	"time"
 
+	apiauth "github.com/harness/gitness/app/api/auth"
+	"github.com/harness/gitness/app/api/controller"
 	"github.com/harness/gitness/app/api/usererror"
 	"github.com/harness/gitness/app/auth"
 	pullreqevents "github.com/harness/gitness/app/events/pullreq"
 	"github.com/harness/gitness/app/services/instrument"
+	"github.com/harness/gitness/errors"
 	"github.com/harness/gitness/git"
 	"github.com/harness/gitness/git/sha"
 	"github.com/harness/gitness/types"
@@ -41,6 +44,8 @@ type CreateInput struct {
 	SourceRepoRef string `json:"source_repo_ref"`
 	SourceBranch  string `json:"source_branch"`
 	TargetBranch  string `json:"target_branch"`
+
+	ReviewerIDs []int64 `json:"reviewer_ids"`
 }
 
 func (in *CreateInput) Sanitize() error {
@@ -124,24 +129,44 @@ func (c *Controller) Create(
 		return nil, fmt.Errorf("failed to fetch PR diff stats: %w", err)
 	}
 
-	targetRepo, err = c.repoStore.UpdateOptLock(ctx, targetRepo, func(repo *types.Repository) error {
-		repo.PullReqSeq++
+	var pr *types.PullReq
+	targetRepoID := targetRepo.ID
+	err = controller.TxOptLock(ctx, c.tx, func(ctx context.Context) error {
+		if targetRepo == nil {
+			targetRepo, err = c.repoStore.Find(ctx, targetRepoID)
+			if err != nil {
+				return fmt.Errorf("failed to increment pullreq sequence number: %w", err)
+			}
+		}
+
+		targetRepo.PullReqSeq++
+		err = c.repoStore.Update(ctx, targetRepo)
+		if err != nil {
+			return fmt.Errorf("failed to update pullreq sequence number: %w", err)
+		}
+
+		pr = newPullReq(session, targetRepo.PullReqSeq, sourceRepo, targetRepo, in, sourceSHA, mergeBaseSHA)
+		pr.Stats = types.PullReqStats{
+			DiffStats:       types.NewDiffStats(prStats.Commits, prStats.FilesChanged, prStats.Additions, prStats.Deletions),
+			Conversations:   0,
+			UnresolvedCount: 0,
+		}
+
+		err = c.pullreqStore.Create(ctx, pr)
+		if err != nil {
+			return fmt.Errorf("pullreq creation failed: %w", err)
+		}
+
+		if err := c.createReviewers(ctx, session, in.ReviewerIDs, targetRepo, pr); err != nil {
+			return err
+		}
+
 		return nil
-	})
+	}, controller.TxOptionResetFunc(func() {
+		targetRepo = nil // on the version conflict error force re-fetch of the target repo
+	}))
 	if err != nil {
-		return nil, fmt.Errorf("failed to acquire PullReqSeq number: %w", err)
-	}
-
-	pr := newPullReq(session, targetRepo.PullReqSeq, sourceRepo, targetRepo, in, sourceSHA, mergeBaseSHA)
-	pr.Stats = types.PullReqStats{
-		DiffStats:       types.NewDiffStats(prStats.Commits, prStats.FilesChanged, prStats.Additions, prStats.Deletions),
-		Conversations:   0,
-		UnresolvedCount: 0,
-	}
-
-	err = c.pullreqStore.Create(ctx, pr)
-	if err != nil {
-		return nil, fmt.Errorf("pullreq creation failed: %w", err)
+		return nil, fmt.Errorf("failed to create pullreq: %w", err)
 	}
 
 	c.eventReporter.Created(ctx, &pullreqevents.CreatedPayload{
@@ -168,6 +193,67 @@ func (c *Controller) Create(
 	}
 
 	return pr, nil
+}
+
+func (c *Controller) createReviewers(
+	ctx context.Context,
+	session *auth.Session,
+	reviewers []int64,
+	repo *types.Repository,
+	pr *types.PullReq,
+) error {
+	if len(reviewers) == 0 {
+		return nil
+	}
+
+	addedByInfo := session.Principal.ToPrincipalInfo()
+	reviewerType := enum.PullReqReviewerTypeRequested
+
+	for _, id := range reviewers {
+		if id == addedByInfo.ID {
+			return usererror.BadRequest("PR creator cannot be added as a reviewer.")
+		}
+
+		reviewerPrincipal, err := c.principalStore.Find(ctx, id)
+		if err != nil {
+			return usererror.BadRequest("Failed to find principal reviewer.")
+		}
+
+		// TODO: To check the reviewer's access to the repo we create a dummy session object. Fix it.
+		if err = apiauth.CheckRepo(
+			ctx,
+			c.authorizer,
+			&auth.Session{
+				Principal: *reviewerPrincipal,
+				Metadata:  nil,
+			},
+			repo,
+			enum.PermissionRepoReview,
+		); err != nil {
+			if errors.Is(err, apiauth.ErrNotAuthorized) {
+				return usererror.BadRequest(
+					"The reviewer doesn't have enough permissions for the repository.",
+				)
+			}
+			return fmt.Errorf(
+				"reviewer principal: %s access error: %w", reviewerPrincipal.UID, err)
+		}
+
+		reviewer := newPullReqReviewer(
+			session, pr, repo,
+			reviewerPrincipal.ToPrincipalInfo(),
+			addedByInfo, reviewerType,
+			&ReviewerAddInput{
+				ReviewerID: id,
+			},
+		)
+
+		if err = c.reviewerStore.Create(ctx, reviewer); err != nil {
+			return fmt.Errorf("failed to create pull request reviewer: %w", err)
+		}
+	}
+
+	return nil
 }
 
 // newPullReq creates new pull request object.
