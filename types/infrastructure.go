d194dc144614f642a42cdab56e45bf82bcb342d7
Vikyath Harekal <vikyath.harekal@harness.io>
2025-01-28 05:06:52 +0000

    feat: [CDE-585]: Add json tags for InstanceInfo (#3331)
    * feat: [CDE-585]: Add json tags for InstanceInfo

--- types/infrastructure.go
+++ types/infrastructure.go
@@ -38,15 +38,16 @@ type PortMapping struct {
 }
 
 type InstanceInfo struct {
-	ID        string
-	Name      string
-	IPAddress string
-	Port      int64
-	OS        string
-	Arch      string
-	Provider  string
-	PoolName  string
-	Zone      string
+	ID                string `json:"id"`
+	Name              string `json:"name"`
+	IPAddress         string `json:"ip_address"`
+	Port              int64  `json:"port"`
+	OS                string `json:"os"`
+	Arch              string `json:"arch"`
+	Provider          string `json:"provider"`
+	PoolName          string `json:"pool_name"`
+	Zone              string `json:"zone"`
+	StorageIdentifier string `json:"storage_identifier"`
 }
 
 type Infrastructure struct {
