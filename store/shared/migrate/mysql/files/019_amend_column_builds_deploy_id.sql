d24e94996a70b29e03671634b6eb83815166bb42
Max Knee <max.knee@nytimes.com>
2025-04-10 15:34:31 -0400

    fixing feedback
    Signed-off-by: Max Knee <max.knee@nytimes.com>

--- store/shared/migrate/mysql/files/019_amend_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/019_amend_column_builds_deploy_id.sql
@@ -1,3 +1,5 @@
 -- name: alter-table-builds-alter-column-deploy-id
-
-ALTER TABLE builds ALTER COLUMN build_deploy_id BIGINT NOT NULL DEFAULT 0;
+ALTER TABLE builds
+ALTER COLUMN build_deploy_id SET DATA TYPE BIGINT,
+ALTER COLUMN build_deploy_id SET NOT NULL,
+ALTER COLUMN build_deploy_id SET DEFAULT 0;
