b1c06a524b236a63cc3c6aeb2be2278609dee050
Tudor Macari <tudor.macari@harness.io>
2025-08-01 13:11:29 +0000

    fix: [AH-1504]: add index on artifacts and bandwidth_stats (#4188)
    * fix: [AH-1504]: add index on artifacts and bandwidth_stats

---
+++ app/store/database/migrate/sqlite/0132_add_indexes_on_images_fk.up.sql
@@ -0,0 +1,3 @@
+create index if not exists idx_artifacts_artifact_image_id ON artifacts (artifact_image_id);
+
+create index if not exists idx_bandwidth_stats_image_id ON bandwidth_stats (bandwidth_stat_image_id);
