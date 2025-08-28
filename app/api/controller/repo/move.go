07be5513895febdef664c4efd177ad92c66e6588
Atefeh Mohseni Ejiyeh <atefeh.mohseni-ejiyeh@harness.io>
2025-08-28 23:36:44 +0000

    feat: [CODE-4380] Support update parent space on repo move api (#4382)

--- app/api/controller/repo/move.go
+++ app/api/controller/repo/move.go
@@ -21,6 +21,8 @@ import (
 	"github.com/harness/gitness/app/auth"
 	"github.com/harness/gitness/types"
 	"github.com/harness/gitness/types/enum"
+
+	"github.com/rs/zerolog/log"
 )
 
 // MoveInput is used for moving a repo.
@@ -28,18 +30,27 @@ type MoveInput struct {
 	// TODO [CODE-1363]: remove after identifier migration.
 	UID        *string `json:"uid" deprecated:"true"`
 	Identifier *string `json:"identifier"`
+	// ParentRef can be either a space ID or space path
+	ParentRef *string `json:"parent_ref"`
 }
 
-func (i *MoveInput) hasChanges(repo *types.Repository) bool {
+func (i *MoveInput) hasChanges(
+	repo *types.Repository,
+	parentSpace *types.SpaceCore,
+	targetParentSpace *types.SpaceCore,
+) bool {
 	if i.Identifier != nil && *i.Identifier != repo.Identifier {
+		return true
+	}
+
+	if i.ParentRef != nil && targetParentSpace.ID != parentSpace.ID {
 		return true
 	}
 
 	return false
 }
 
-// Move moves a repository to a new identifier.
-// TODO: Add support for moving to other parents and aliases.
+// Move moves a repository to a new identifier and/or parent space.
 //
 //nolint:gocognit // refactor if needed
 func (c *Controller) Move(ctx context.Context,
@@ -61,7 +72,20 @@ func (c *Controller) Move(ctx context.Context,
 		return nil, fmt.Errorf("failed to find repo by ID: %w", err)
 	}
 
-	if !in.hasChanges(repo) {
+	currentParentSpace, err := c.spaceFinder.FindByID(ctx, repo.ParentID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find current parent space: %w", err)
+	}
+
+	targetParentSpace := currentParentSpace
+	if in.ParentRef != nil {
+		targetParentSpace, err = c.getSpaceCheckAuthRepoCreation(ctx, session, *in.ParentRef)
+		if err != nil {
+			return nil, fmt.Errorf("failed to access target parent space: %w", err)
+		}
+	}
+
+	if !in.hasChanges(repo, currentParentSpace, targetParentSpace) {
 		return GetRepoOutput(ctx, c.publicAccess, repo)
 	}
 
@@ -80,9 +104,12 @@ func (c *Controller) Move(ctx context.Context,
 	}
 
 	// TODO add a repo level lock here to avoid racing condition or partial repo update w/o setting repo public access
-	renamedRepo, err := c.repoStore.UpdateOptLock(ctx, repo, func(r *types.Repository) error {
+	movedRepo, err := c.repoStore.UpdateOptLock(ctx, repo, func(r *types.Repository) error {
 		if in.Identifier != nil {
 			r.Identifier = *in.Identifier
+		}
+		if targetParentSpace.ID != r.ParentID {
+			r.ParentID = targetParentSpace.ID
 		}
 		return nil
 	})
@@ -94,16 +121,17 @@ func (c *Controller) Move(ctx context.Context,
 	c.repoFinder.MarkChanged(ctx, repo.Core())
 
 	// set public access for the new repo path
-	if err := c.publicAccess.Set(ctx, enum.PublicResourceTypeRepo, renamedRepo.Path, isPublic); err != nil {
+	if err := c.publicAccess.Set(ctx, enum.PublicResourceTypeRepo, movedRepo.Path, isPublic); err != nil {
 		// ensure public access for new repo path is cleaned up first or we risk leaking it
-		if dErr := c.publicAccess.Delete(ctx, enum.PublicResourceTypeRepo, renamedRepo.Path); dErr != nil {
+		if dErr := c.publicAccess.Delete(ctx, enum.PublicResourceTypeRepo, movedRepo.Path); dErr != nil {
 			return nil, fmt.Errorf("failed to set repo public access (and public access cleanup: %w): %w", dErr, err)
 		}
 
-		// revert identifier changes first
+		// revert identifier and parent changes first
 		var dErr error
-		_, dErr = c.repoStore.UpdateOptLock(ctx, renamedRepo, func(r *types.Repository) error {
+		_, dErr = c.repoStore.UpdateOptLock(ctx, movedRepo, func(r *types.Repository) error {
 			r.Identifier = repo.Identifier
+			r.ParentID = repo.ParentID
 			return nil
 		})
 		if dErr != nil {
@@ -115,7 +143,7 @@ func (c *Controller) Move(ctx context.Context,
 		}
 
 		// clear updated repo from cache
-		c.repoFinder.MarkChanged(ctx, renamedRepo.Core())
+		c.repoFinder.MarkChanged(ctx, movedRepo.Core())
 
 		// revert public access changes only after we successfully restored original path
 		if dErr = c.publicAccess.Set(ctx, enum.PublicResourceTypeRepo, repo.Path, isPublic); dErr != nil {
@@ -129,10 +157,15 @@ func (c *Controller) Move(ctx context.Context,
 		return nil, fmt.Errorf("failed to set repo public access for new path (cleanup successful): %w", err)
 	}
 
-	renamedRepo.GitURL = c.urlProvider.GenerateGITCloneURL(ctx, renamedRepo.Path)
-	renamedRepo.GitSSHURL = c.urlProvider.GenerateGITCloneSSHURL(ctx, renamedRepo.Path)
+	movedRepo.GitURL = c.urlProvider.GenerateGITCloneURL(ctx, movedRepo.Path)
+	movedRepo.GitSSHURL = c.urlProvider.GenerateGITCloneSSHURL(ctx, movedRepo.Path)
 
-	return GetRepoOutput(ctx, c.publicAccess, renamedRepo)
+	// TODO: add audit log
+	log.Ctx(ctx).Info().Msgf(
+		"Moved repository %s to %s operation perofrmed by %s",
+		repo.Path, movedRepo.Path, session.Principal.Email)
+
+	return GetRepoOutput(ctx, c.publicAccess, movedRepo)
 }
 
 func (c *Controller) sanitizeMoveInput(in *MoveInput, session *auth.Session) error {
@@ -144,6 +177,12 @@ func (c *Controller) sanitizeMoveInput(in *MoveInput, session *auth.Session) err
 	if in.Identifier != nil {
 		if err := c.identifierCheck(*in.Identifier, session); err != nil {
 			return err
+		}
+	}
+
+	if in.ParentRef != nil {
+		if err := ValidateParentRef(*in.ParentRef); err != nil {
+			return fmt.Errorf("invalid space reference: %w", err)
 		}
 	}
 
