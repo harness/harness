6f2baa687e556fba456f13ece387e632ce7de566
Deepak Bhatt <deepak.bhatt@harness.io>
2025-09-03 09:55:08 +0000

    feat: [CDE-1040]: always return disableSSH field for IDE (#4443)
    * feat: [CDE-1040]: always return disableSSH field for IDE

--- types/gitspace_settings.go
+++ types/gitspace_settings.go
@@ -94,7 +94,7 @@ func (c GitspaceSettingsCriteria) ToKey() (CriteriaKey, error) {
 
 type IDESettings struct {
 	AccessList *AccessList[enum.IDEType] `json:"access_list,omitempty"` // Access control for IDEs
-	DisableSSH bool                      `json:"disable_ssh,omitempty"`
+	DisableSSH bool                      `json:"disable_ssh"`
 }
 
 type AccessList[T comparable] struct {
